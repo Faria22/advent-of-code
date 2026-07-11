6f1abb2a4cde26b722e99af63fa669eb45e55b6f
Faria22 <fariafelipe22@hotmail.com>
2026-07-10 21:48:46 -0500

    Solve 2025 day 2 in Python

--- 2025/day02/python/solution.py
+++ 2025/day02/python/solution.py
@@ -1,24 +1,49 @@
 from pathlib import Path
 
+INPUT_PATH = Path(__file__).parent.parent / 'input.txt'
 
-INPUT_PATH = Path(__file__).parent.parent / "input.txt"
+
+def is_invalid(number: int, count: int = -1) -> bool:
+    pattern = ''
+    str_number = str(number)
+    for i in str_number[:-1]:  # skips the last character
+        pattern += i
+        if not str_number.replace(pattern, '', count):
+            return True
+    return False
 
 
-def part_one(data: str):
-    """Return the answer to part one."""
-    return None
+def parse_range(id_range: str) -> range:
+    start, end = id_range.split('-')
+    start = int(start)
+    end = int(end)
+    return range(start, end + 1)
+
+
+def part_one(data: str) -> int:
+    total = 0
+    for id_range in data.split(','):
+        for number in parse_range(id_range):
+            if is_invalid(number, 2):
+                total += number
+    return total
 
 
 def part_two(data: str):
     """Return the answer to part two."""
-    return None
+    total = 0
+    for id_range in data.split(','):
+        for number in parse_range(id_range):
+            if is_invalid(number):
+                total += number
+    return total
 
 
 def main() -> None:
-    data = INPUT_PATH.read_text().rstrip("\n")
-    print(f"Part 1: {part_one(data)}")
-    print(f"Part 2: {part_two(data)}")
+    data = INPUT_PATH.read_text().rstrip('\n')
+    print(f'Part 1: {part_one(data)}')
+    print(f'Part 2: {part_two(data)}')
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     main()
