b5835b736216660c3d82e70fe028bc201d75b411
Faria22 <fariafelipe22@hotmail.com>
2026-07-12 09:07:52 -0500

    solution 2025 day 4 part 1

--- years/2025/day04/solution.py
+++ years/2025/day04/solution.py
@@ -1,24 +1,65 @@
 from pathlib import Path
 
+import numpy as np
 
-INPUT_PATH = Path(__file__).parent / "input.txt"
+INPUT_PATH = Path(__file__).parent / 'input.txt'
 
 
-def part_one(data: str):
+SIZE = 136
+
+
+def parse_input(data: str) -> np.ndarray:
+    grid = np.zeros((SIZE, SIZE), dtype=bool)
+    for row_idx, row in enumerate(data.split('\n')):
+        for roll_idx, roll in enumerate(row):
+            if roll == '@':
+                grid[row_idx, roll_idx] = True
+
+    return grid
+
+
+def sum_surrounding(row: np.intp, col: np.intp, grid: np.ndarray) -> int:
+    left = col - 1
+    right = col + 1
+    up = row + 1
+    down = row - 1
+
+    # Get all surrounding indices
+    inds = [(up, left), (row, left), (down, left), (up, col), (down, col), (up, right), (row, right), (down, right)]
+
+    # Makes sure the indices are inbound
+    inds = [ind for ind in inds if all(0 <= i < SIZE for i in ind)]
+
+    rows, cols = zip(*inds, strict=True)
+    return np.sum(grid[rows, cols])
+
+
+def get_surrounding_rolls(grid: np.ndarray) -> np.ndarray:
+    result = np.full_like(grid, -1, dtype=int)
+
+    rows, cols = np.where(grid)  # only the (r, c) values where grid is True (where there is a roll)
+    for r, c in zip(rows, cols, strict=True):
+        result[r, c] = sum_surrounding(r, c, grid)
+    return result
+
+
+def part_one(grid: np.ndarray) -> np.intp:
     """Return the answer to part one."""
-    return None
+    return np.sum((grid < 4) & (grid >= 0))  # ruff:ignore[magic-value-comparison]
 
 
-def part_two(data: str):
+def part_two(grid: np.ndarray) -> np.intp:
     """Return the answer to part two."""
-    return None
+    return np.sum(np.where((grid < 4) & (grid >= 0)))  # ruff:ignore[magic-value-comparison]
 
 
 def main() -> None:
-    data = INPUT_PATH.read_text().rstrip("\n")
-    print(f"Part 1: {part_one(data)}")
-    print(f"Part 2: {part_two(data)}")
+    data = INPUT_PATH.read_text().rstrip('\n')
+    grid = parse_input(data)
+    grid = get_surrounding_rolls(grid)
+    print(f'Part 1: {part_one(grid)}')
+    print(f'Part 2: {part_two(grid)}')
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     main()
--- years/2025/day04/test.py
+++ years/2025/day04/test.py
@@ -1,1 +1,17 @@
 # ruff: noqa: PLR2004
+import numpy as np
+from solution import sum_surrounding
+
+
+def test_sum_surrounding() -> None:
+    grid = np.array([[True, False, True], [True, True, True], [False, False, False]])
+
+    assert sum_surrounding(0, 0, grid) == 2
+    assert sum_surrounding(0, 1, grid) == 5
+    assert sum_surrounding(0, 2, grid) == 2
+    assert sum_surrounding(1, 0, grid) == 2
+    assert sum_surrounding(1, 1, grid) == 4
+    assert sum_surrounding(1, 2, grid) == 2
+    assert sum_surrounding(2, 0, grid) == 2
+    assert sum_surrounding(2, 1, grid) == 3
+    assert sum_surrounding(2, 2, grid) == 2
