dbf2b71dcdf42b6fed0d70a2b2d4b66e03465c3a
Faria22 <fariafelipe22@hotmail.com>
2026-07-12 21:52:05 -0500

    Solve 2025 day 6 part 1 in Python

--- years/2025/day06/solution.py
+++ years/2025/day06/solution.py
@@ -1,24 +1,46 @@
+import re
 from pathlib import Path
 
+import numpy as np
 
-INPUT_PATH = Path(__file__).parent / "input.txt"
+INPUT_PATH = Path(__file__).parent / 'input.txt'
 
 
-def part_one(data: str):
+def part_one(nums: np.ndarray, ops: np.ndarray) -> int:
     """Return the answer to part one."""
-    return None
+    ret = []
+    for i, o in enumerate(ops):
+        if o == '+':
+            ret.append(np.sum(nums[:, i]))
+        else:
+            ret.append(np.prod(nums[:, i]))
+    return sum(ret)
 
 
 def part_two(data: str):
     """Return the answer to part two."""
-    return None
+    return
+
+
+def parse_data(data: str) -> tuple[np.ndarray, ...]:
+    all_white_space = re.compile(r'[^\S\n]+')
+    clean = re.sub(all_white_space, ',', data.strip())
+
+    clean_path = Path('clean_input.txt')
+    clean_path.write_text(clean)
+
+    grid = np.loadtxt(clean_path, delimiter=',', dtype=object)
+    nums = grid[:-1, :].astype(int)
+    ops = grid[-1, :]
+    return nums, ops
 
 
 def main() -> None:
-    data = INPUT_PATH.read_text().rstrip("\n")
-    print(f"Part 1: {part_one(data)}")
-    print(f"Part 2: {part_two(data)}")
+    data = INPUT_PATH.read_text().rstrip('\n')
+    nums, ops = parse_data(data)
+    print(f'Part 1: {part_one(nums, ops)}')
+    print(f'Part 2: {part_two(data)}')
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     main()
