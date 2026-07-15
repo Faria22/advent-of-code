4319a7e9dec27475d4caed573228cc2dc3819042
Faria22 <fariafelipe22@hotmail.com>
2026-07-14 20:50:54 -0500

    Solve 2025 day 7

--- years/2025/day07/solution.py
+++ years/2025/day07/solution.py
@@ -1,24 +1,88 @@
+from copy import deepcopy
 from pathlib import Path
 
+INPUT_PATH = Path(__file__).parent / 'input.txt'
 
-INPUT_PATH = Path(__file__).parent / "input.txt"
+
+START_POINT = 'S'
+LASER_CHAR = '|'
+BLANK_CHAR = '.'
+SPLITTER_CHAR = '^'
+
+type Grid = list[list[str]]
 
 
-def part_one(data: str):
+def parse_data(data: str) -> Grid:
+    grid = [list(line) for line in data.split('\n')]
+    grid[0] = [cell.replace(START_POINT, LASER_CHAR) for cell in grid[0]]
+
+    return grid
+
+
+def part_one(grid: Grid) -> int:
     """Return the answer to part one."""
-    return None
+    split_times = 0
+    for i, row in enumerate(grid[1:], 1):
+        for j, cell in enumerate(row):
+            # Do nothing if there is no laser in the previous row
+            if grid[i - 1][j] != LASER_CHAR:
+                continue
+
+            if cell == BLANK_CHAR:
+                grid[i][j] = LASER_CHAR
+            elif cell == SPLITTER_CHAR:
+                split_times += 1
+                grid[i][j - 1] = LASER_CHAR
+                grid[i][j + 1] = LASER_CHAR
+
+    return split_times
 
 
-def part_two(data: str):
+def quantum_split(grid: Grid, current_row: int, cache: dict[tuple[int, int], int]) -> int:
+    timeline_count = 0
+    for i, row in enumerate(grid[current_row:], current_row):
+        for j, cell in enumerate(row):
+            # Do nothing if there is no laser in the previous row
+            if grid[i - 1][j] != LASER_CHAR:
+                continue
+
+            if cell == BLANK_CHAR:
+                grid[i][j] = LASER_CHAR
+            elif cell == SPLITTER_CHAR:
+                if (i, j) in cache:
+                    return cache[i, j]
+
+                left_split_grid = deepcopy(grid)
+                left_split_grid[i][j - 1] = LASER_CHAR
+                timeline_count += quantum_split(left_split_grid, i + 1, cache)
+
+                right_split_grid = deepcopy(grid)
+                right_split_grid[i][j + 1] = LASER_CHAR
+                timeline_count += quantum_split(right_split_grid, i + 1, cache)
+
+                # Caches answer if a bean hits the same splitter again
+                cache[i, j] = timeline_count
+                return timeline_count
+
+    # Adds one for when it gets to the end
+    # The idea is that if there were no splitters then
+    # we would still have a single timeline
+    return timeline_count + 1
+
+
+def part_two(grid: Grid) -> int:
     """Return the answer to part two."""
-    return None
+
+    cache: dict[tuple[int, int], int] = {}
+    return quantum_split(grid, 1, cache)
 
 
 def main() -> None:
-    data = INPUT_PATH.read_text().rstrip("\n")
-    print(f"Part 1: {part_one(data)}")
-    print(f"Part 2: {part_two(data)}")
+    data = INPUT_PATH.read_text().rstrip('\n')
+    grid = parse_data(data)
+    print(f'Part 1: {part_one(deepcopy(grid))}')
+    print(f'Part 2: {part_two(deepcopy(grid))}')
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     main()
--- years/2025/day07/test.py
+++ years/2025/day07/test.py
@@ -1,1 +1,16 @@
 # ruff: noqa: PLR2004
+from pathlib import Path
+
+from solution import parse_data, part_one, part_two
+
+
+def test_part_one_solution_against_example() -> None:
+    input_path = Path(__file__).parent / 'puzzle_input.txt'
+    grid = parse_data(input_path.read_text().rstrip('\n'))
+    assert part_one(grid) == 21
+
+
+def test_part_two_solution_against_example() -> None:
+    input_path = Path(__file__).parent / 'puzzle_input.txt'
+    grid = parse_data(input_path.read_text().rstrip('\n'))
+    assert part_two(grid) == 40
