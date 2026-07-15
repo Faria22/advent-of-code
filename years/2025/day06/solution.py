18bd704789e721225c2d0eb8bdb97da7d01f8df5
Faria22 <fariafelipe22@hotmail.com>
2026-07-14 19:51:01 -0500

    solution to 2025 day 6 part 2

--- years/2025/day06/solution.py
+++ years/2025/day06/solution.py
@@ -1,4 +1,5 @@
 import re
+from math import prod
 from pathlib import Path
 
 import numpy as np
@@ -6,23 +7,30 @@ import numpy as np
 INPUT_PATH = Path(__file__).parent / 'input.txt'
 
 
-def part_one(nums: np.ndarray, ops: np.ndarray) -> int:
+def part_one(data: str) -> int:
     """Return the answer to part one."""
-    ret = []
+    nums, ops = parse_data_part_one(data)
+    column_totals = []
     for i, o in enumerate(ops):
         if o == '+':
-            ret.append(np.sum(nums[:, i]))
+            column_totals.append(np.sum(nums[:, i]))
         else:
-            ret.append(np.prod(nums[:, i]))
-    return sum(ret)
+            column_totals.append(np.prod(nums[:, i]))
+    return sum(column_totals)
 
 
-def part_two(data: str):
-    """Return the answer to part two."""
-    return
+def part_two(data: str) -> int:
+    nums, ops = parse_data_part_two(data)
+    column_totals = []
+    for i, o in enumerate(ops):
+        if o == '+':
+            column_totals.append(sum(nums[i]))
+        else:
+            column_totals.append(prod(nums[i]))
+    return sum(column_totals)
 
 
-def parse_data(data: str) -> tuple[np.ndarray, ...]:
+def parse_data_part_one(data: str) -> tuple[np.ndarray, np.ndarray]:
     all_white_space = re.compile(r'[^\S\n]+')
     clean = re.sub(all_white_space, ',', data.strip())
 
@@ -35,10 +43,34 @@ def parse_data(data: str) -> tuple[np.ndarray, ...]:
     return nums, ops
 
 
+def parse_data_part_two(data: str) -> tuple[list[list[int]], list[str]]:
+    # transpose nums
+    nums = data.split('\n')[:-1]
+    ops = [op.strip() for op in data.rsplit('\n', maxsplit=1)[-1].split() if op.strip()]
+
+    len_nums = len(nums[0])  # number of number (counting spaces)
+    transposed_nums = [''] * len_nums  # Creates a row for each column
+    for i in range(len_nums):
+        for row in nums:
+            transposed_nums[i] += row[i]
+
+    transposed_nums = [num.strip() for num in transposed_nums]
+    transposed_grid = []
+    col = []
+    for num in transposed_nums:
+        if num:
+            col.append(int(num))
+        else:
+            transposed_grid.append(col)
+            col = []
+    transposed_grid.append(col)
+
+    return transposed_grid, ops
+
+
 def main() -> None:
     data = INPUT_PATH.read_text().rstrip('\n')
-    nums, ops = parse_data(data)
-    print(f'Part 1: {part_one(nums, ops)}')
+    print(f'Part 1: {part_one(data)}')
     print(f'Part 2: {part_two(data)}')
 
 
