01c1d0c42f6721d4691be6276d8a1b091858df64
Faria22 <fariafelipe22@hotmail.com>
2026-07-26 11:38:16 -0500

    solution 2024 day 1

--- years/2024/day01/solution.py
+++ years/2024/day01/solution.py
@@ -1,24 +1,40 @@
 from pathlib import Path
 
+import numpy as np
 
-INPUT_PATH = Path(__file__).parent / "input.txt"
+INPUT_PATH = Path(__file__).parent / 'input.txt'
 
 
-def part_one(data: str):
+def parse_data(input_file: Path) -> tuple[np.ndarray, np.ndarray]:
+    lists = np.loadtxt(input_file, dtype=int)
+    list_1 = np.sort(lists[:, 0].ravel())
+    list_2 = np.sort(lists[:, 1].ravel())
+    return list_1, list_2
+
+
+def part_one(input_path: Path) -> int:
     """Return the answer to part one."""
-    return None
+
+    list_1, list_2 = parse_data(input_path)
+
+    diff = np.abs(list_1 - list_2)
+    return np.sum(diff)
 
 
-def part_two(data: str):
+def part_two(input_path: Path) -> int:
     """Return the answer to part two."""
-    return None
+    list_1, list_2 = parse_data(input_path)
+
+    matches = list_1[:, np.newaxis] == list_2
+    occurrences = matches.sum(axis=1)
+
+    return list_1 @ occurrences
 
 
 def main() -> None:
-    data = INPUT_PATH.read_text().rstrip("\n")
-    print(f"Part 1: {part_one(data)}")
-    print(f"Part 2: {part_two(data)}")
+    print(f'Part 1: {part_one(INPUT_PATH)}')
+    print(f'Part 2: {part_two(INPUT_PATH)}')
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     main()
