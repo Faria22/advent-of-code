357d8aecbca6cb24d9bfe2f4152b26a75ed18833
Faria22 <fariafelipe22@hotmail.com>
2026-07-26 23:10:16 -0500

    solution 2024 day 3

--- years/2024/day03/solution.py
+++ years/2024/day03/solution.py
@@ -1,24 +1,46 @@
+import re
 from pathlib import Path
 
+INPUT_PATH = Path(__file__).parent / 'input.txt'
 
-INPUT_PATH = Path(__file__).parent / "input.txt"
+
+def parse_data(input_path: Path) -> str:
+    return input_path.read_text().replace('\n', '')
 
 
-def part_one(data: str):
+def part_one(input_path: Path) -> int:
     """Return the answer to part one."""
-    return None
+    pattern = r'mul\((\d{1,3}),(\d{1,3})\)'
+    data = parse_data(input_path)
+    matches = re.findall(pattern, data)
+
+    return sum(int(a) * int(b) for a, b in matches)
 
 
-def part_two(data: str):
+def part_two(input_path: Path) -> int:
     """Return the answer to part two."""
-    return None
+    pattern = r"mul\((\d{1,3}),(\d{1,3})\)|(do\(\)|don't\(\))"
+    data = parse_data(input_path)
+    matches = re.findall(pattern, data)
+
+    total = 0
+    on = True
+    for a, b, c in matches:
+        if on and c == "don't()":
+            on = False
+        elif not on and c == 'do()':
+            on = True
+
+        if on and a and b:
+            total += int(a) * int(b)
+
+    return total
 
 
 def main() -> None:
-    data = INPUT_PATH.read_text().rstrip("\n")
-    print(f"Part 1: {part_one(data)}")
-    print(f"Part 2: {part_two(data)}")
+    print(f'Part 1: {part_one(INPUT_PATH)}')
+    print(f'Part 2: {part_two(INPUT_PATH)}')
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     main()
--- years/2024/day03/test.py
+++ years/2024/day03/test.py
@@ -1,1 +1,15 @@
 # ruff: file-ignore[magic-value-comparison]
+
+from pathlib import Path
+
+from solution import part_one, part_two
+
+
+def test_part_one_with_sample_input() -> None:
+    sample_path = Path(__file__).parent / 'sample_input_1.txt'
+    assert part_one(sample_path) == 161
+
+
+def test_part_two_with_sample_input() -> None:
+    sample_path = Path(__file__).parent / 'sample_input_2.txt'
+    assert part_two(sample_path) == 48
