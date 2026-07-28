6e8eb1be515bdec372cbababb02598acc87cddfd
Faria22 <fariafelipe22@hotmail.com>
2026-07-28 15:17:42 -0500

    solution 2024 day 7

--- years/2024/day07/solution.py
+++ years/2024/day07/solution.py
@@ -1,22 +1,70 @@
+from collections.abc import Iterable
+from itertools import product
 from pathlib import Path
+from typing import Literal, NamedTuple
 
 INPUT_PATH = Path(__file__).parent / 'input.txt'
 
+PLUS = '+'
+MULT = '*'
+CONC = '||'
+type Operation = Literal['+', '*', '||']
 
-def parse_data(input_path: Path) -> str:
-    return input_path.read_text().strip()
+
+class Test(NamedTuple):
+    expected_value: int
+    numbers: list[int]
+
+    def solvable(self, operation_set: set[Operation]) -> bool:
+        for operations in product(operation_set, repeat=len(self.numbers) - 1):
+            if self.expected_value == solve_expression(self.numbers.copy(), operations):
+                return True
+        return False
+
+
+def solve_expression(numbers: list[int], operations: Iterable[Operation]) -> int:
+    total = numbers.pop(0)
+    for number, operation in zip(numbers, operations, strict=True):
+        if operation == PLUS:
+            total += number
+        elif operation == MULT:
+            total *= number
+        elif operation == CONC:
+            total = int(str(total) + str(number))
+        else:
+            raise ValueError
+
+    return total
+
+
+def parse_data(input_path: Path) -> list[Test]:
+    tests = []
+    for line in input_path.read_text().strip().split('\n'):
+        expected_value, numbers = line.split(': ')
+        numbers = [int(number) for number in numbers.split()]
+        tests.append(Test(int(expected_value), numbers))
+
+    return tests
 
 
 def part_one(input_path: Path) -> int:
     """Return the answer to part one."""
-    data = parse_data(input_path)  # ruff: ignore[unused-variable]
-    return 0
+    tests = parse_data(input_path)
+    total = 0
+    for test in tests:
+        if test.solvable({PLUS, MULT}):
+            total += test.expected_value
+    return total
 
 
 def part_two(input_path: Path) -> int:
     """Return the answer to part two."""
-    data = parse_data(input_path)  # ruff: ignore[unused-variable]
-    return 0
+    tests = parse_data(input_path)
+    total = 0
+    for test in tests:
+        if test.solvable({PLUS, MULT, CONC}):
+            total += test.expected_value
+    return total
 
 
 def main() -> None:
--- years/2024/day07/test.py
+++ years/2024/day07/test.py
@@ -1,13 +1,29 @@
+# ruff: file-ignore[magic-value-comparison]
 from pathlib import Path
 
-from solution import part_one, part_two
+from solution import part_one, part_two, solve_expression
 
 SAMPLE_PATH = Path(__file__).parent / 'sample_input.txt'
 
 
 def test_part_one_with_sample_input() -> None:
-    assert part_one(SAMPLE_PATH) == 0
+    assert part_one(SAMPLE_PATH) == 3749
 
 
 def test_part_two_with_sample_input() -> None:
-    assert part_two(SAMPLE_PATH) == 0
+    assert part_two(SAMPLE_PATH) == 11387
+
+
+def test_solve_expression() -> None:
+    assert solve_expression([10, 20], ['+']) == 30
+    assert solve_expression([10, 20], ['*']) == 200
+    assert solve_expression([10, 20], ['||']) == 1020
+    assert solve_expression([10, 20, 30], ['+', '+']) == 60
+    assert solve_expression([10, 20, 30], ['+', '*']) == 900
+    assert solve_expression([10, 20, 30], ['+', '||']) == 3030
+    assert solve_expression([10, 20, 30], ['*', '+']) == 230
+    assert solve_expression([10, 20, 30], ['*', '*']) == 6000
+    assert solve_expression([10, 20, 30], ['*', '||']) == 20030
+    assert solve_expression([10, 20, 30], ['||', '+']) == 1050
+    assert solve_expression([10, 20, 30], ['||', '*']) == 30600
+    assert solve_expression([10, 20, 30], ['||', '||']) == 102030
