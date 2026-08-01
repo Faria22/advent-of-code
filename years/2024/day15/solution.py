ed029196e751e6b08ea45661effc18ae5f69a51b
Faria22 <fariafelipe22@hotmail.com>
2026-08-01 13:08:19 -0500

    solution 2024 day 15 part 1

--- years/2024/day15/solution.py
+++ years/2024/day15/solution.py
@@ -1,16 +1,95 @@
+from dataclasses import dataclass
 from pathlib import Path
+
+from aoc import Pos
 
 INPUT_PATH = Path(__file__).parent / 'input.txt'
 
+WALL_CHAR = '#'
+BOX_CHAR = 'O'
+ROBOT_CHAR = '@'
 
-def parse_data(input_path: Path) -> list[str]:
-    return input_path.read_text().strip().split('\n')
+MOVE_OFFSETS = {
+    '^': Pos(-1, 0),
+    'v': Pos(1, 0),
+    '<': Pos(0, -1),
+    '>': Pos(0, 1),
+}
+
+
+@dataclass
+class Warehouse:
+    robot: Pos
+    walls: set[Pos]
+    boxes: set[Pos]
+    moves: str
+
+    def move_boxes(self, move: str) -> tuple[set[Pos], set[Pos]]:
+        offset = MOVE_OFFSETS[move]
+        position = self.robot + offset
+        moving_boxes = set()
+
+        # Get all boxes that are "connected" to the robot
+        while position in self.boxes:
+            moving_boxes.add(position)
+            position += offset
+
+        new_positions = {box + offset for box in moving_boxes}
+
+        return moving_boxes, new_positions
+
+    def move_robot(self, move: str) -> None:
+        offset = MOVE_OFFSETS[move]
+        new_robot_pos = self.robot + offset
+        if new_robot_pos in self.walls:
+            return
+
+        # Get all the boxes that would be shifted
+        # and find their new position
+        moving_boxes, moved_boxes_new_positions = self.move_boxes(move)
+
+        # Stop if any box would go into a wall
+        if any(box in self.walls for box in moved_boxes_new_positions):
+            return
+
+        self.boxes -= moving_boxes
+        self.boxes |= moved_boxes_new_positions
+        self.robot = new_robot_pos
+
+    def execute_all_moves(self) -> None:
+        for move in self.moves:
+            self.move_robot(move)
+
+    @staticmethod
+    def gps_coordinate(box: Pos) -> int:
+        return 100 * box.row + box.col
+
+    def sum_all_gps_coordinates(self) -> int:
+        return sum(self.gps_coordinate(box) for box in self.boxes)
+
+
+def parse_data(input_path: Path) -> Warehouse:
+    grid, moves = input_path.read_text().strip().split('\n\n')
+
+    walls, boxes = set(), set()
+    for r, row in enumerate(grid.split('\n')):
+        for c, cell in enumerate(row):
+            if cell == WALL_CHAR:
+                walls.add(Pos(r, c))
+            elif cell == BOX_CHAR:
+                boxes.add(Pos(r, c))
+            elif cell == ROBOT_CHAR:
+                robot_pos = Pos(r, c)
+
+    moves = moves.replace('\n', '')
+    return Warehouse(robot_pos, walls, boxes, moves)
 
 
 def part_one(input_path: Path) -> int:
     """Return the answer to part one."""
-    data = parse_data(input_path)  # ruff: ignore[unused-variable]
-    return 0
+    warehouse = parse_data(input_path)
+    warehouse.execute_all_moves()
+    return warehouse.sum_all_gps_coordinates()
 
 
 def part_two(input_path: Path) -> int:
--- years/2024/day15/test.py
+++ years/2024/day15/test.py
@@ -1,14 +1,23 @@
-# ruff: file-ignore[magic-value-comparison]  # ruff: ignore[unused-noqa]
+# ruff: file-ignore[magic-value-comparison]
 from pathlib import Path
 
-from solution import part_one, part_two
+from aoc import Pos
+from solution import Warehouse, part_one, part_two
 
-SAMPLE_PATH = Path(__file__).parent / 'sample_input.txt'
-
-
-def test_part_one_with_sample_input() -> None:
-    assert part_one(SAMPLE_PATH) == 0
+DIR = Path(__file__).parent
 
 
-def test_part_two_with_sample_input() -> None:
-    assert part_two(SAMPLE_PATH) == 0
+def test_part_one_with_sample_input1() -> None:
+    assert part_one(DIR / 'sample_input1.txt') == 10092
+
+
+def test_part_one_with_sample_input2() -> None:
+    assert part_one(DIR / 'sample_input2.txt') == 2028
+
+
+def test_part_two_with_sample_input1() -> None:
+    assert part_two(DIR / 'sample_input1.txt') == 9021
+
+
+def test_gps_coordinate() -> None:
+    assert Warehouse.gps_coordinate(Pos(1, 4)) == 104
