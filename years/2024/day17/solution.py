132840b3461f4b668aaefda2d3babd3912d9fe60
Faria22 <fariafelipe22@hotmail.com>
2026-08-02 12:57:08 -0500

    solution 2024 day 17

--- years/2024/day17/solution.py
+++ years/2024/day17/solution.py
@@ -1,22 +1,120 @@
+from dataclasses import dataclass
 from pathlib import Path
 
 INPUT_PATH = Path(__file__).parent / 'input.txt'
 
 
-def parse_data(input_path: Path) -> list[str]:
-    return input_path.read_text().strip().split('\n')
+@dataclass
+class Computer:
+    def __init__(self, a: int, b: int, c: int, program: list[int]) -> None:
+        self.a = a
+        self.b = b
+        self.c = c
+        self.program = program
+        self.ins_pointer = 0
+
+    @staticmethod
+    def _literal(val: int) -> int:
+        return val
+
+    def _combo(self, val: int) -> int:
+        match val:
+            case x if 0 <= x <= 3:  # ruff: ignore[magic-value-comparison]
+                return val
+            case 4:
+                return self.a
+            case 5:
+                return self.b
+            case 6:
+                return self.c
+            case _:
+                raise ValueError
+
+    def _adv(self, inp: int) -> int:
+        return self.a // 2 ** self._combo(inp)
+
+    def _jump(self, val: int) -> None:
+        self.ins_pointer = val
+
+    def _run_instruction(self) -> str | None:
+        ins = self.program[self.ins_pointer]
+        inp = self.program[self.ins_pointer + 1]
+
+        output = None
+        match ins:
+            case 0:
+                self.a = self._adv(inp)
+            case 1:
+                self.b ^= self._literal(inp)
+            case 2:
+                self.b = self._combo(inp) % 8
+            case 3:
+                if self.a != 0:
+                    self._jump(inp)
+                    return None
+            case 4:
+                self.b ^= self.c
+            case 5:
+                output = str(self._combo(inp) % 8)
+            case 6:
+                self.b = self._adv(inp)
+            case 7:
+                self.c = self._adv(inp)
+
+        self.ins_pointer += 2
+        return output
+
+    def run_program(self) -> str:
+        len_program = len(self.program)
+        final_output = []
+        while self.ins_pointer < len_program:
+            output = self._run_instruction()
+            if output is not None:
+                final_output.append(output)
+
+        return ','.join(final_output)
 
 
-def part_one(input_path: Path) -> int:
+def parse_data(input_path: Path) -> Computer:
+    a, b, c, _, p = input_path.read_text().strip().split('\n')
+
+    a = int(a.split()[-1])
+    b = int(b.split()[-1])
+    c = int(c.split()[-1])
+
+    p = p.split()[-1].split(',')
+    p = [int(x) for x in p]
+
+    return Computer(a, b, c, p)
+
+
+def part_one(input_path: Path) -> str:
     """Return the answer to part one."""
-    data = parse_data(input_path)  # ruff: ignore[unused-variable]
-    return 0
+    computer = parse_data(input_path)
+    return computer.run_program()
+
+
+def test(start) -> int: ...
 
 
 def part_two(input_path: Path) -> int:
     """Return the answer to part two."""
-    data = parse_data(input_path)  # ruff: ignore[unused-variable]
-    return 0
+    computer = parse_data(input_path)
+    expected_program = ''.join(str(x) for x in computer.program)
+
+    # a must be zero at the end of the program for it to hault
+    a_possible_values = [0]
+    for i in range(1, len(computer.program) + 1):
+        new_a_possible_values = [
+            8 * a_possible_value + digit for a_possible_value in a_possible_values for digit in range(8)
+        ]
+        a_possible_values = []
+        for a_val in new_a_possible_values:
+            output = Computer(a_val, computer.b, computer.c, computer.program).run_program().replace(',', '')
+            if output[-i:] == expected_program[-i:]:
+                a_possible_values.append(a_val)
+
+    return min(a_possible_values)
 
 
 def main() -> None:
--- years/2024/day17/test.py
+++ years/2024/day17/test.py
@@ -1,14 +1,37 @@
-# ruff: file-ignore[magic-value-comparison]  # ruff: ignore[unused-noqa]
+# ruff: file-ignore[magic-value-comparison]
 from pathlib import Path
 
-from solution import part_one, part_two
+from solution import Computer, part_one, part_two
 
-SAMPLE_PATH = Path(__file__).parent / 'sample_input.txt'
+DIR = Path(__file__).parent
+
+
+def test_simple_programs() -> None:
+    computer = Computer(0, 0, 9, [2, 6])
+    output = computer.run_program()
+    assert computer.b == 1
+
+    computer = Computer(10, 0, 0, [5, 0, 5, 1, 5, 4])
+    output = computer.run_program()
+    assert output == '0,1,2'
+
+    computer = Computer(2024, 0, 0, [0, 1, 5, 4, 3, 0])
+    output = computer.run_program()
+    assert output == '4,2,5,6,7,7,7,7,3,1,0'
+    assert computer.a == 0
+
+    computer = Computer(0, 29, 0, [1, 7])
+    output = computer.run_program()
+    assert computer.b == 26
+
+    computer = Computer(0, 2024, 43690, [4, 0])
+    output = computer.run_program()
+    assert computer.b == 44354
 
 
 def test_part_one_with_sample_input() -> None:
-    assert part_one(SAMPLE_PATH) == 0
+    assert part_one(DIR / 'sample_input1.txt') == '4,6,3,5,6,3,5,2,1,0'
 
 
 def test_part_two_with_sample_input() -> None:
-    assert part_two(SAMPLE_PATH) == 0
+    assert part_two(DIR / 'sample_input2.txt') == 117440
