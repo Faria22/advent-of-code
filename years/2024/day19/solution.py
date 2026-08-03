e008baf5f0231a5298844a540b0fac43944728c9
Faria22 <fariafelipe22@hotmail.com>
2026-08-03 18:54:21 -0500

    solution 2024 day 19

--- years/2024/day19/solution.py
+++ years/2024/day19/solution.py
@@ -1,22 +1,50 @@
+from functools import cache
 from pathlib import Path
 
 INPUT_PATH = Path(__file__).parent / 'input.txt'
 
 
-def parse_data(input_path: Path) -> list[str]:
-    return input_path.read_text().strip().split('\n')
+def parse_data(input_path: Path) -> tuple[tuple[str, ...], list[str]]:
+    patterns, designs = input_path.read_text().strip().split('\n\n')
+
+    patterns = tuple(patterns.split(', '))
+    designs = designs.split('\n')
+
+    return patterns, designs
+
+
+@cache
+def can_be_built(design: str, patterns: tuple[str, ...]) -> int:
+    if not design:
+        return 1
+
+    num_options = 0
+    for pattern in patterns:
+        if design.startswith(pattern):
+            num_options += can_be_built(design[len(pattern) :], patterns)
+
+    return num_options
 
 
 def part_one(input_path: Path) -> int:
     """Return the answer to part one."""
-    data = parse_data(input_path)  # ruff: ignore[unused-variable]
-    return 0
+    patterns, designs = parse_data(input_path)
+
+    possible_designs_count = 0
+    for design in designs:
+        if can_be_built(design, patterns):
+            possible_designs_count += 1
+    return possible_designs_count
 
 
 def part_two(input_path: Path) -> int:
     """Return the answer to part two."""
-    data = parse_data(input_path)  # ruff: ignore[unused-variable]
-    return 0
+    patterns, designs = parse_data(input_path)
+
+    num_options = 0
+    for design in designs:
+        num_options += can_be_built(design, patterns)
+    return num_options
 
 
 def main() -> None:
