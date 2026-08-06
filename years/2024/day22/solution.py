192ca4501a0b92fb358efa792df3d9ca102cb73a
Faria22 <fariafelipe22@hotmail.com>
2026-08-05 21:11:35 -0500

    solution 2024 day 22

--- years/2024/day22/solution.py
+++ years/2024/day22/solution.py
@@ -1,22 +1,59 @@
+from collections import defaultdict
 from pathlib import Path
+
+import numpy as np
 
 INPUT_PATH = Path(__file__).parent / 'input.txt'
 
 
-def parse_data(input_path: Path) -> list[str]:
-    return input_path.read_text().strip().split('\n')
+def parse_data(input_path: Path) -> list[int]:
+    lines = input_path.read_text().strip().split('\n')
+    return [int(line) for line in lines]
+
+
+def process(secret_number: int, n: int = 1) -> int:
+    for _ in range(n):
+        number = secret_number * 64
+        secret_number = (secret_number ^ number) % 16777216
+        number = secret_number // 32
+        secret_number = (secret_number ^ number) % 16777216
+        number = secret_number * 2048
+        secret_number = (secret_number ^ number) % 16777216
+    return secret_number
 
 
 def part_one(input_path: Path) -> int:
     """Return the answer to part one."""
-    data = parse_data(input_path)  # ruff: ignore[unused-variable]
-    return 0
+    total = 0
+    for number in parse_data(input_path):
+        total += process(number, 2000)
+    return total
 
 
 def part_two(input_path: Path) -> int:
     """Return the answer to part two."""
-    data = parse_data(input_path)  # ruff: ignore[unused-variable]
-    return 0
+    numbers = parse_data(input_path)
+    prices = np.zeros((len(numbers), 2001), dtype=int)
+    for i, num in enumerate(numbers):
+        prices[i, 0] = num % 10
+        cur_num = num
+        for j in range(2000):
+            cur_num = process(cur_num)
+            prices[i, j + 1] = cur_num % 10
+
+    price_diffs = np.diff(prices)
+    print(price_diffs.shape)
+    bananas_per_diff = defaultdict(int)
+    for i in range(len(price_diffs)):
+        seen_keys = set()
+        for j in range(4, 2001):
+            key = tuple(price_diffs[i, j - 4 : j].tolist())
+            if key in seen_keys:
+                continue
+
+            seen_keys.add(key)
+            bananas_per_diff[key] += int(prices[i, j])
+    return max(bananas_per_diff.values())
 
 
 def main() -> None:
--- years/2024/day22/test.py
+++ years/2024/day22/test.py
@@ -1,14 +1,34 @@
-# ruff: file-ignore[magic-value-comparison]  # ruff: ignore[unused-noqa]
+# ruff: file-ignore[magic-value-comparison]
 from pathlib import Path
 
-from solution import part_one, part_two
+from solution import part_one, part_two, process
 
-SAMPLE_PATH = Path(__file__).parent / 'sample_input.txt'
+DIR = Path(__file__).parent
 
 
 def test_part_one_with_sample_input() -> None:
-    assert part_one(SAMPLE_PATH) == 0
+    assert part_one(DIR / 'sample_input1.txt') == 37327623
 
 
 def test_part_two_with_sample_input() -> None:
-    assert part_two(SAMPLE_PATH) == 0
+    assert part_two(DIR / 'sample_input2.txt') == 23
+
+
+def test_process() -> None:
+    assert process(123) == 15887950
+    assert process(15887950) == 16495136
+    assert process(16495136) == 527345
+    assert process(527345) == 704524
+    assert process(704524) == 1553684
+    assert process(1553684) == 12683156
+    assert process(12683156) == 11100544
+    assert process(11100544) == 12249484
+    assert process(12249484) == 7753432
+    assert process(7753432) == 5908254
+
+
+def test_2000_process() -> None:
+    assert process(1, 2000) == 8685429
+    assert process(10, 2000) == 4700978
+    assert process(100, 2000) == 15273692
+    assert process(2024, 2000) == 8667524
