733d2f9a56c832e9ab9eac0cde5f8cea5e833601
Faria22 <fariafelipe22@hotmail.com>
2026-08-05 20:08:31 -0500

    solution 2024 day 21

--- years/2024/day21/solution.py
+++ years/2024/day21/solution.py
@@ -1,22 +1,157 @@
+from functools import cache
+from itertools import permutations, product
 from pathlib import Path
 
+from aoc import Pos as BasePos
+
 INPUT_PATH = Path(__file__).parent / 'input.txt'
+
+
+class Pos(BasePos):
+    def move(self, move: str) -> 'Pos':
+        match move:
+            case '^':
+                return self.shift_up()
+            case 'v':
+                return self.shift_down()
+            case '<':
+                return self.shift_left()
+            case '>':
+                return self.shift_right()
+            case _:
+                raise ValueError(f'{move} is not a valid move')
+
+
+class BasePad:
+    def __init__(self, buttons: dict[str, Pos], start_button: str = 'A') -> None:
+        self.buttons = buttons
+        assert start_button in buttons
+        self.cur_pos: Pos = buttons[start_button]
+
+    def press_button(self, button: str) -> set[str]:
+        if button not in self.buttons:
+            raise ValueError('Not an available button')
+
+        end_button_pos = self.buttons[button]
+
+        vertical_shift, horizontal_shift = end_button_pos - self.cur_pos
+
+        vertical_shift_str = '^' if vertical_shift < 0 else 'v'
+        horizontal_shift_str = '<' if horizontal_shift < 0 else '>'
+
+        moves = vertical_shift_str * abs(vertical_shift) + horizontal_shift_str * abs(horizontal_shift)
+
+        possible_moves: set[str] = set()
+        for move_sequence in permutations(moves, len(moves)):
+            cur_pos = self.cur_pos
+            for move in move_sequence:
+                cur_pos = cur_pos.move(move)
+                if cur_pos not in self.buttons.values():
+                    break
+            else:
+                possible_moves.add(''.join(move_sequence) + 'A')
+
+        self.cur_pos = end_button_pos
+        return possible_moves
+
+
+class DirPad(BasePad):
+    def __init__(self, start_button: str) -> None:
+        buttons = {
+            '^': Pos(0, 1),
+            'A': Pos(0, 2),
+            'v': Pos(1, 1),
+            '<': Pos(1, 0),
+            '>': Pos(1, 2),
+        }
+        super().__init__(buttons, start_button)
+
+
+class NumPad(BasePad):
+    def __init__(self, start_button: str) -> None:
+        buttons = {
+            '7': Pos(0, 0),
+            '8': Pos(0, 1),
+            '9': Pos(0, 2),
+            '4': Pos(1, 0),
+            '5': Pos(1, 1),
+            '6': Pos(1, 2),
+            '1': Pos(2, 0),
+            '2': Pos(2, 1),
+            '3': Pos(2, 2),
+            '0': Pos(3, 1),
+            'A': Pos(3, 2),
+        }
+        super().__init__(buttons, start_button)
+
+
+@cache
+def lowest_cost_move(pad_class: type[DirPad | NumPad], start: str, end: str, remaining_layers: int) -> int:
+    pad = pad_class(start)
+
+    possible_moves = pad.press_button(end)
+    if remaining_layers == 0:
+        move = possible_moves.pop()
+        return len(move)
+
+    costs = []
+    for possible_move in possible_moves:
+        cost = 0
+        prev_button = 'A'
+        for button in possible_move:
+            cur_cost = lowest_cost_move(DirPad, prev_button, button, remaining_layers - 1)
+            cost += cur_cost
+            prev_button = button
+        costs.append(cost)
+
+    return min(costs)
 
 
 def parse_data(input_path: Path) -> list[str]:
     return input_path.read_text().strip().split('\n')
 
 
+def complexity(num_button_presses: int, code: str) -> int:
+    num_code = int(code[:-1])
+    return num_code * num_button_presses
+
+
+def get_move_sequences_from_possible_moves(moves: list[set[str]]) -> list[str]:
+    move_sequences = list(product(*moves))
+    return [''.join(move_sequence) for move_sequence in move_sequences]
+
+
+def get_final_num_moves(code: str, num_layers: int) -> int:
+    prev_button = 'A'
+    total_cost = 0
+    for button in code:
+        total_cost += lowest_cost_move(NumPad, prev_button, button, num_layers)
+        prev_button = button
+    return total_cost
+
+
 def part_one(input_path: Path) -> int:
     """Return the answer to part one."""
-    data = parse_data(input_path)  # ruff: ignore[unused-variable]
-    return 0
+    codes = parse_data(input_path)
+
+    total_complexity = 0
+    for code in codes:
+        num_moves = get_final_num_moves(code, 2)
+        total_complexity += complexity(num_moves, code)
+
+    return total_complexity
 
 
 def part_two(input_path: Path) -> int:
     """Return the answer to part two."""
-    data = parse_data(input_path)  # ruff: ignore[unused-variable]
-    return 0
+    codes = parse_data(input_path)
+
+    total_complexity = 0
+    for code in codes:
+        num_moves = get_final_num_moves(code, 25)
+        total_complexity += complexity(num_moves, code)
+
+    return total_complexity
 
 
 def main() -> None:
--- years/2024/day21/test.py
+++ years/2024/day21/test.py
@@ -1,14 +1,28 @@
-# ruff: file-ignore[magic-value-comparison]  # ruff: ignore[unused-noqa]
+# ruff: file-ignore[magic-value-comparison]
 from pathlib import Path
 
-from solution import part_one, part_two
+from solution import DirPad, NumPad, lowest_cost_move, part_one
 
 SAMPLE_PATH = Path(__file__).parent / 'sample_input.txt'
 
 
 def test_part_one_with_sample_input() -> None:
-    assert part_one(SAMPLE_PATH) == 0
+    assert part_one(SAMPLE_PATH) == 126384
 
 
-def test_part_two_with_sample_input() -> None:
-    assert part_two(SAMPLE_PATH) == 0
+def test_lowest_cost_move_without_remaining_layers() -> None:
+    cost = lowest_cost_move(NumPad, 'A', 'A', 0)
+    assert cost == 1
+
+
+def test_lowest_cost_move_accounts_for_starting_button() -> None:
+    forward_cost = lowest_cost_move(DirPad, 'A', '<', 1)
+    reverse_cost = lowest_cost_move(DirPad, '<', 'A', 1)
+
+    assert forward_cost == 10
+    assert reverse_cost == 8
+
+
+def test_lowest_cost_move_selects_best_three_to_seven_path() -> None:
+    cost = lowest_cost_move(NumPad, '3', '7', 2)
+    assert cost == 23
