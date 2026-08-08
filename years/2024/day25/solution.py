6f0cd0a3ceb1be87b62e22bdc3e87ff7c086f840
Faria22 <fariafelipe22@hotmail.com>
2026-08-07 21:09:26 -0500

    solution 2024 day 25

--- years/2024/day25/solution.py
+++ years/2024/day25/solution.py
@@ -1,16 +1,45 @@
+from itertools import product
 from pathlib import Path
 
 INPUT_PATH = Path(__file__).parent / 'input.txt'
+NUM_HEIGHTS = 5
+MAX_HEIGHT = 5
+
+type Key = tuple[int, ...]
+type Lock = tuple[int, ...]
 
 
-def parse_data(input_path: Path) -> list[str]:
-    return input_path.read_text().strip().split('\n')
+def parse_heights(lines: list[str]) -> tuple[int, ...]:
+    transposed_data = [[line[i] for line in lines] for i in range(NUM_HEIGHTS)]
+    return tuple(line.count('#') for line in transposed_data)
+
+
+def parse_data(input_path: Path) -> tuple[set[Key], set[Lock]]:
+    keys = set()
+    locks = set()
+    lines = input_path.read_text().strip().split('\n')
+    line_idx = 0
+    while line_idx < len(lines):
+        first_line = lines[line_idx]
+        info_lines = lines[line_idx + 1 : line_idx + 6]
+        if first_line.startswith('.'):
+            keys.add(parse_heights(info_lines))
+        else:
+            locks.add(parse_heights(info_lines))
+
+        line_idx += 8
+
+    return keys, locks
 
 
 def part_one(input_path: Path) -> int:
     """Return the answer to part one."""
-    data = parse_data(input_path)  # ruff: ignore[unused-variable]
-    return 0
+    keys, locks = parse_data(input_path)
+    count = 0
+    for key, lock in product(keys, locks):
+        if all(k + l <= MAX_HEIGHT for k, l in zip(key, lock, strict=True)):
+            count += 1
+    return count
 
 
 def part_two(input_path: Path) -> int:
