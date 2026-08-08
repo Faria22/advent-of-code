7575810963eaa8b70d5f83d2027ad1b2c45b1c5a
Faria22 <fariafelipe22@hotmail.com>
2026-08-07 21:38:20 -0500

    solution 2015 day 3

--- years/2015/day03/solution.py
+++ years/2015/day03/solution.py
@@ -1,22 +1,40 @@
+from itertools import batched
 from pathlib import Path
+
+from aoc import Pos
 
 INPUT_PATH = Path(__file__).parent / 'input.txt'
 
 
-def parse_data(input_path: Path) -> list[str]:
-    return input_path.read_text().strip().split('\n')
+def parse_data(input_path: Path) -> str:
+    return input_path.read_text().strip()
 
 
 def part_one(input_path: Path) -> int:
     """Return the answer to part one."""
-    data = parse_data(input_path)  # ruff: ignore[unused-variable]
-    return 0
+    moves = parse_data(input_path)
+    cur_pos = Pos(0, 0)
+    visited = {cur_pos}
+    for move in moves:
+        cur_pos = cur_pos.move(move)
+        visited.add(cur_pos)
+
+    return len(visited)
 
 
 def part_two(input_path: Path) -> int:
     """Return the answer to part two."""
-    data = parse_data(input_path)  # ruff: ignore[unused-variable]
-    return 0
+    moves = parse_data(input_path)
+    santa_pos = Pos(0, 0)
+    robot_pos = Pos(0, 0)
+    visited = {santa_pos}
+    for s_m, r_m in batched(moves, 2):
+        santa_pos = santa_pos.move(s_m)
+        robot_pos = robot_pos.move(r_m)
+        visited.add(santa_pos)
+        visited.add(robot_pos)
+
+    return len(visited)
 
 
 def main() -> None:
