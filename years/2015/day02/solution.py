0a0311d26382ca2ef927e1d13547c0d2e58d1a1e
Faria22 <fariafelipe22@hotmail.com>
2026-08-07 21:21:39 -0500

    solution 2015 day 2

--- years/2015/day02/solution.py
+++ years/2015/day02/solution.py
@@ -3,20 +3,41 @@ from pathlib import Path
 INPUT_PATH = Path(__file__).parent / 'input.txt'
 
 
-def parse_data(input_path: Path) -> list[str]:
-    return input_path.read_text().strip().split('\n')
+def parse_data(input_path: Path) -> list[tuple[int, int, int]]:
+    lines = input_path.read_text().strip().split('\n')
+
+    dimensions = []
+    for line in lines:
+        x, y, z = line.split('x')
+        dimensions.append((int(x), int(y), int(z)))
+
+    return dimensions
+
+
+def required_paper(dimentions: tuple[int, int, int]) -> int:
+    x, y, z = dimentions
+    area = 2 * x * y + 2 * x * z + 2 * y * z
+    extra = min(x * y, x * z, y * z)
+    return area + extra
+
+
+def required_ribbon(dimentions: tuple[int, int, int]) -> int:
+    x, y, z = dimentions
+    volume = x * y * z
+    extra = 2 * min(x + y, x + z, y + z)
+    return volume + extra
 
 
 def part_one(input_path: Path) -> int:
     """Return the answer to part one."""
-    data = parse_data(input_path)  # ruff: ignore[unused-variable]
-    return 0
+    dimentions = parse_data(input_path)
+    return sum(required_paper(dim) for dim in dimentions)
 
 
 def part_two(input_path: Path) -> int:
     """Return the answer to part two."""
-    data = parse_data(input_path)  # ruff: ignore[unused-variable]
-    return 0
+    dimentions = parse_data(input_path)
+    return sum(required_ribbon(dim) for dim in dimentions)
 
 
 def main() -> None:
