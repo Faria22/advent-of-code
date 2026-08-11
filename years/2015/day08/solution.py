9507d7015bb779bf745501568f3912eac3f930a0
Faria22 <fariafelipe22@hotmail.com>
2026-08-10 21:42:20 -0500

    solution 2015 day 8

--- years/2015/day08/solution.py
+++ years/2015/day08/solution.py
@@ -7,16 +7,24 @@ def parse_data(input_path: Path) -> list[str]:
     return input_path.read_text().strip().split('\n')
 
 
+def parse_string(string: str) -> str:
+    return string.encode().decode('unicode_escape')[1:-1]
+
+
+def encode_string(string: str) -> str:
+    return f'"{string.replace("\\", "\\\\").replace('"', r"\"")}"'
+
+
 def part_one(input_path: Path) -> int:
     """Return the answer to part one."""
-    data = parse_data(input_path)  # ruff: ignore[unused-variable]
-    return 0
+    lines = parse_data(input_path)
+    return sum(len(line) - len(parse_string(line)) for line in lines)
 
 
 def part_two(input_path: Path) -> int:
     """Return the answer to part two."""
-    data = parse_data(input_path)  # ruff: ignore[unused-variable]
-    return 0
+    lines = parse_data(input_path)
+    return sum(len(encode_string(line)) - len(line) for line in lines)
 
 
 def main() -> None:
