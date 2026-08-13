0bacb9694f1992ee3935f8823fe099d7ffd37d70
Faria22 <fariafelipe22@hotmail.com>
2026-08-12 23:07:05 -0500

    solution 2015 day 11

--- years/2015/day11/solution.py
+++ years/2015/day11/solution.py
@@ -2,21 +2,73 @@ from pathlib import Path
 
 INPUT_PATH = Path(__file__).parent / 'input.txt'
 
+BAD_CHARS_INDS = (ord('i'), ord('l'), ord('o'))
+MIN_IND = ord('a')
+MAX_IND = ord('z')
 
-def parse_data(input_path: Path) -> list[str]:
-    return input_path.read_text().strip().split('\n')
+
+def parse_data(input_path: Path) -> str:
+    return input_path.read_text().strip()
 
 
-def part_one(input_path: Path) -> int:
+def allowed_password(password: list[int]) -> bool:
+    if any(ind in password for ind in BAD_CHARS_INDS):
+        return False
+
+    if not any(
+        char + 1 == password[idx + 1] and char + 2 == password[idx + 2] for idx, char in enumerate(password[:-2])
+    ):
+        return False
+
+    idx = 0
+    pair_count = 0
+    while idx < len(password) - 1:
+        if password[idx] == password[idx + 1]:
+            pair_count += 1
+            idx += 1
+        idx += 1
+
+    return pair_count > 1
+
+
+def increase_password(password: list[int]) -> None:
+    len_password = len(password)
+
+    for idx in reversed(range(len_password)):
+        password[idx] += 1
+        if password[idx] > MAX_IND:
+            password[idx] = MIN_IND
+        else:
+            break
+
+    # Skips bad letters fast
+    for idx in range(len_password):
+        if password[idx] in BAD_CHARS_INDS:
+            password[idx] += 1
+            for j in range(idx + 1, len_password):
+                password[j] = MIN_IND
+            break
+
+
+def part_one(input_path: Path) -> str:
     """Return the answer to part one."""
-    data = parse_data(input_path)  # ruff: ignore[unused-variable]
-    return 0
+    initial_password = parse_data(input_path)
+    password = [ord(char) for char in initial_password]
+    increase_password(password)
+    while not allowed_password(password):
+        increase_password(password)
+    while not allowed_password(password):
+        increase_password(password)
+    return ''.join(chr(p) for p in password)
 
 
-def part_two(input_path: Path) -> int:
+def part_two(input_path: Path) -> str:
     """Return the answer to part two."""
-    data = parse_data(input_path)  # ruff: ignore[unused-variable]
-    return 0
+    password = [ord(char) for char in 'cqjxxyzz']
+    increase_password(password)
+    while not allowed_password(password):
+        increase_password(password)
+    return ''.join(chr(p) for p in password)
 
 
 def main() -> None:
--- years/2015/day11/test.py
+++ years/2015/day11/test.py
@@ -1,14 +1,16 @@
-# ruff: file-ignore[magic-value-comparison]  # ruff: ignore[unused-noqa]
 from pathlib import Path
 
-from solution import part_one, part_two
+from solution import allowed_password, part_one
 
-SAMPLE_PATH = Path(__file__).parent / 'sample_input.txt'
+DIR = Path(__file__).parent
 
 
 def test_part_one_with_sample_input() -> None:
-    assert part_one(SAMPLE_PATH) == 0
+    assert part_one(DIR / 'sample_input1.txt') == 'abcdffaa'
+    assert part_one(DIR / 'sample_input2.txt') == 'ghjaabcc'
 
 
-def test_part_two_with_sample_input() -> None:
-    assert part_two(SAMPLE_PATH) == 0
+def test_allowed_password() -> None:
+    assert not allowed_password([ord(p) for p in 'hijklmmn'])
+    assert not allowed_password([ord(p) for p in 'abbceffg'])
+    assert not allowed_password([ord(p) for p in 'abbcegjk'])
