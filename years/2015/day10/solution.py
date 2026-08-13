2b56eab7227b0d19a393e7f7c98a08c7e059c48f
Faria22 <fariafelipe22@hotmail.com>
2026-08-12 21:30:27 -0500

    solution 2015 day 10

--- years/2015/day10/solution.py
+++ years/2015/day10/solution.py
@@ -3,20 +3,42 @@ from pathlib import Path
 INPUT_PATH = Path(__file__).parent / 'input.txt'
 
 
-def parse_data(input_path: Path) -> list[str]:
-    return input_path.read_text().strip().split('\n')
+def parse_data(input_path: Path) -> str:
+    return input_path.read_text().strip()
 
 
-def part_one(input_path: Path) -> int:
+def step(string: str) -> str:
+    if len(string) == 1:
+        return f'1{string}'
+
+    prev_char = string[0]
+    count = 1
+    new_string = ''
+    for char in string[1:]:
+        if char != prev_char:
+            new_string += f'{count}{prev_char}'
+            count = 1
+        else:
+            count += 1
+        prev_char = char
+    new_string += f'{count}{prev_char}'
+    return new_string
+
+
+def part_one(input_path: Path, steps: int = 40) -> int:
     """Return the answer to part one."""
-    data = parse_data(input_path)  # ruff: ignore[unused-variable]
-    return 0
+    start = parse_data(input_path)
+    for _ in range(steps):
+        start = step(start)
+    return len(start)
 
 
 def part_two(input_path: Path) -> int:
     """Return the answer to part two."""
-    data = parse_data(input_path)  # ruff: ignore[unused-variable]
-    return 0
+    start = parse_data(input_path)
+    for _ in range(50):
+        start = step(start)
+    return len(start)
 
 
 def main() -> None:
--- years/2015/day10/test.py
+++ years/2015/day10/test.py
@@ -1,14 +1,14 @@
-# ruff: file-ignore[magic-value-comparison]  # ruff: ignore[unused-noqa]
+# ruff: file-ignore[magic-value-comparison]
 from pathlib import Path
 
-from solution import part_one, part_two
+from solution import part_one
 
 SAMPLE_PATH = Path(__file__).parent / 'sample_input.txt'
 
 
 def test_part_one_with_sample_input() -> None:
-    assert part_one(SAMPLE_PATH) == 0
-
-
-def test_part_two_with_sample_input() -> None:
-    assert part_two(SAMPLE_PATH) == 0
+    assert part_one(SAMPLE_PATH, 1) == 2
+    assert part_one(SAMPLE_PATH, 2) == 2
+    assert part_one(SAMPLE_PATH, 3) == 4
+    assert part_one(SAMPLE_PATH, 4) == 6
+    assert part_one(SAMPLE_PATH, 5) == 6
