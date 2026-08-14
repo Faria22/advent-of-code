6058305a7d8c509cb61d536c22c02829f77a166e
Faria22 <fariafelipe22@hotmail.com>
2026-08-13 19:22:02 -0500

    solution 2015 day 12

--- years/2015/day12/solution.py
+++ years/2015/day12/solution.py
@@ -1,22 +1,48 @@
+import json
+import re
 from pathlib import Path
+from typing import Any
 
 INPUT_PATH = Path(__file__).parent / 'input.txt'
 
 
-def parse_data(input_path: Path) -> list[str]:
-    return input_path.read_text().strip().split('\n')
+def parse_data(input_path: Path) -> str:
+    return input_path.read_text().strip()
+
+
+def get_sum(content: str) -> int:
+    numbers = re.findall(r'(-?\d+)', content)
+    return sum(int(num) for num in numbers)
 
 
 def part_one(input_path: Path) -> int:
     """Return the answer to part one."""
-    data = parse_data(input_path)  # ruff: ignore[unused-variable]
-    return 0
+    line = parse_data(input_path)
+    return get_sum(line)
+
+
+def walk_json(value: Any) -> int:
+    total = 0
+    if isinstance(value, dict):
+        if 'red' in value or 'red' in value.values():
+            return total
+
+        for item in value.values():
+            total += walk_json(item)
+
+    elif isinstance(value, list):
+        for item in value:
+            total += walk_json(item)
+    elif isinstance(value, int):
+        total += value
+    return total
 
 
 def part_two(input_path: Path) -> int:
     """Return the answer to part two."""
-    data = parse_data(input_path)  # ruff: ignore[unused-variable]
-    return 0
+    line = parse_data(input_path)
+    data = json.loads(line)
+    return walk_json(data)
 
 
 def main() -> None:
--- years/2015/day12/test.py
+++ years/2015/day12/test.py
@@ -1,13 +1,23 @@
 # ruff: file-ignore[magic-value-comparison]  # ruff: ignore[unused-noqa]
 from pathlib import Path
 
-from solution import part_one, part_two
+from solution import get_sum, part_two
 
 SAMPLE_PATH = Path(__file__).parent / 'sample_input.txt'
 
 
 def test_part_one_with_sample_input() -> None:
-    assert part_one(SAMPLE_PATH) == 0
+    assert get_sum('[1,2,3]') == 6
+    assert get_sum('{"a":2,"b":4}') == 6
+
+    assert get_sum('[[[3]]]') == 3
+    assert get_sum('{"a":{"b":4},"c":-1}') == 3
+
+    assert get_sum('{"a":[-1,1]}') == 0
+    assert get_sum('[-1,{"a":1}]') == 0
+
+    assert get_sum('[]') == 0
+    assert get_sum('{}') == 0
 
 
 def test_part_two_with_sample_input() -> None:
