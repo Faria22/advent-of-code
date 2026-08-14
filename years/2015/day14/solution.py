fbc0c9e3c487b54296c171734d4fcd5aac32071d
Faria22 <fariafelipe22@hotmail.com>
2026-08-13 20:24:16 -0500

    solution 2015 day 14

--- years/2015/day14/solution.py
+++ years/2015/day14/solution.py
@@ -1,22 +1,52 @@
+import re
 from pathlib import Path
+from typing import NamedTuple
 
 INPUT_PATH = Path(__file__).parent / 'input.txt'
 
 
-def parse_data(input_path: Path) -> list[str]:
-    return input_path.read_text().strip().split('\n')
+class Reindeer(NamedTuple):
+    speed: int
+    travel_time: int
+    rest_time: int
+
+    def total_travel_distance(self, time: int) -> int:
+        time_per_cycle = self.travel_time + self.rest_time
+
+        num_cycles, remaining_time = divmod(time, time_per_cycle)
+
+        extra_travel_time = min(remaining_time, self.travel_time)
+
+        return self.speed * (num_cycles * self.travel_time + extra_travel_time)
 
 
-def part_one(input_path: Path) -> int:
+def parse_data(input_path: Path) -> list[Reindeer]:
+    reindeers = []
+    for line in input_path.read_text().strip().split('\n'):
+        a, b, c = re.findall(r'\d+', line)
+        reindeers.append(Reindeer(int(a), int(b), int(c)))
+
+    return reindeers
+
+
+def part_one(input_path: Path, seconds: int = 2503) -> int:
     """Return the answer to part one."""
-    data = parse_data(input_path)  # ruff: ignore[unused-variable]
-    return 0
+    reindeers = parse_data(input_path)
+    return max(reindeer.total_travel_distance(seconds) for reindeer in reindeers)
 
 
-def part_two(input_path: Path) -> int:
+def part_two(input_path: Path, seconds: int = 2503) -> int:
     """Return the answer to part two."""
-    data = parse_data(input_path)  # ruff: ignore[unused-variable]
-    return 0
+    reindeers = parse_data(input_path)
+    points = [0] * len(reindeers)
+    for t in range(1, seconds + 1):
+        distances = [reindeer.total_travel_distance(t) for reindeer in reindeers]
+        max_d = max(distances)
+        for idx, d in enumerate(distances):
+            if d == max_d:
+                points[idx] += 1
+
+    return max(points)
 
 
 def main() -> None:
