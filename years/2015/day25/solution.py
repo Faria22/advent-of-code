fb645ad38b12538d9d56aecc880cf50416be6afd
Faria22 <fariafelipe22@hotmail.com>
2026-08-18 13:43:06 -0500

    solution 2015 day 25

--- years/2015/day25/solution.py
+++ years/2015/day25/solution.py
@@ -1,27 +1,35 @@
-from pathlib import Path
-
-INPUT_PATH = Path(__file__).parent / 'input.txt'
-
-
-def parse_data(input_path: Path) -> list[str]:
-    return input_path.read_text().strip().split('\n')
+ROW = 2947
+COL = 3029
 
 
-def part_one(input_path: Path) -> int:
+def get_idx_for_row_col(row: int, col: int) -> int:
+    idx_at_row_one = col * (col + 1) // 2  # Idx on the first row but correct_col
+
+    row_shift = row * (row - 1) // 2
+    if row_shift:
+        row_shift += (col - 1) * (row - 1)
+    return idx_at_row_one + row_shift
+
+
+def part_one(row: int = ROW, col: int = COL) -> int:
     """Return the answer to part one."""
-    data = parse_data(input_path)  # ruff: ignore[unused-variable]
-    return 0
+    idx = get_idx_for_row_col(row, col)
+    val = 20151125
+    for _ in range(idx - 1):
+        val *= 252533
+        val %= 33554393
+
+    return val
 
 
-def part_two(input_path: Path) -> int:
+def part_two() -> int:
     """Return the answer to part two."""
-    data = parse_data(input_path)  # ruff: ignore[unused-variable]
     return 0
 
 
 def main() -> None:
-    print(f'Part 1: {part_one(INPUT_PATH)}')
-    print(f'Part 2: {part_two(INPUT_PATH)}')
+    print(f'Part 1: {part_one()}')
+    print(f'Part 2: {part_two()}')
 
 
 if __name__ == '__main__':
--- years/2015/day25/test.py
+++ years/2015/day25/test.py
@@ -1,14 +1,27 @@
-# ruff: file-ignore[magic-value-comparison]  # ruff: ignore[unused-noqa]
+# ruff: file-ignore[magic-value-comparison]
 from pathlib import Path
 
-from solution import part_one, part_two
+from solution import get_idx_for_row_col, part_one
 
 SAMPLE_PATH = Path(__file__).parent / 'sample_input.txt'
 
 
-def test_part_one_with_sample_input() -> None:
-    assert part_one(SAMPLE_PATH) == 0
+def test_get_idx_for_row_col() -> None:
+    assert get_idx_for_row_col(1, 1) == 1
+    assert get_idx_for_row_col(2, 1) == 2
+    assert get_idx_for_row_col(3, 1) == 4
+    assert get_idx_for_row_col(4, 1) == 7
+    assert get_idx_for_row_col(1, 2) == 3
+    assert get_idx_for_row_col(1, 3) == 6
+    assert get_idx_for_row_col(1, 4) == 10
+    assert get_idx_for_row_col(1, 5) == 15
+    assert get_idx_for_row_col(3, 3) == 13
+    assert get_idx_for_row_col(4, 3) == 18
 
 
-def test_part_two_with_sample_input() -> None:
-    assert part_two(SAMPLE_PATH) == 0
+def test_part_one() -> None:
+    assert part_one(1, 1) == 20151125
+    assert part_one(2, 1) == 31916031
+    assert part_one(3, 1) == 16080970
+    assert part_one(1, 3) == 17289845
+    assert part_one(3, 3) == 1601130
