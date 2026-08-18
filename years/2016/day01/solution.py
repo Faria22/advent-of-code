41a479329a2e3fd055c36813ada73bed46e31b9d
Faria22 <fariafelipe22@hotmail.com>
2026-08-18 18:24:56 -0500

    solution 2016 day 1

--- years/2016/day01/solution.py
+++ years/2016/day01/solution.py
@@ -1,21 +1,76 @@
 from pathlib import Path
 
+from aoc import Pos
+
 INPUT_PATH = Path(__file__).parent / 'input.txt'
+
+RIGHT = {
+    'n': 'e',
+    'e': 's',
+    's': 'w',
+    'w': 'n',
+}
+LEFT = {
+    'n': 'w',
+    'w': 's',
+    's': 'e',
+    'e': 'n',
+}
 
 
 def parse_data(input_path: Path) -> list[str]:
-    return input_path.read_text().strip().split('\n')
+    return input_path.read_text().strip().split(', ')
 
 
 def part_one(input_path: Path) -> int:
     """Return the answer to part one."""
-    data = parse_data(input_path)  # ruff: ignore[unused-variable]
-    return 0
+    moves = parse_data(input_path)
+    pos = Pos(0, 0)
+    direction = 'n'
+    for move in moves:
+        side, num = move[0], int(move[1:])
+        direction = RIGHT[direction] if side == 'R' else LEFT[direction]
+        match direction:
+            case 'n':
+                pos = pos.shift_up(num)
+            case 's':
+                pos = pos.shift_down(num)
+            case 'e':
+                pos = pos.shift_right(num)
+            case 'w':
+                pos = pos.shift_left(num)
+
+    return sum(abs(p) for p in pos)
 
 
 def part_two(input_path: Path) -> int:
     """Return the answer to part two."""
-    data = parse_data(input_path)  # ruff: ignore[unused-variable]
+    moves = parse_data(input_path)
+    pos = Pos(0, 0)
+    seen = set()
+    direction = 'n'
+    for move in moves:
+        side, num = move[0], int(move[1:])
+        direction = RIGHT[direction] if side == 'R' else LEFT[direction]
+        match direction:
+            case 'n':
+                visited = {pos.shift_up(n) for n in range(1, num + 1)}
+                pos = pos.shift_up(num)
+            case 's':
+                visited = {pos.shift_down(n) for n in range(1, num + 1)}
+                pos = pos.shift_down(num)
+            case 'e':
+                visited = {pos.shift_right(n) for n in range(1, num + 1)}
+                pos = pos.shift_right(num)
+            case 'w':
+                visited = {pos.shift_left(n) for n in range(1, num + 1)}
+                pos = pos.shift_left(num)
+
+        for v_pos in visited:
+            if v_pos in seen:
+                return sum(abs(p) for p in v_pos)
+
+        seen.update(visited)
     return 0
 
 
--- years/2016/day01/test.py
+++ years/2016/day01/test.py
@@ -1,14 +1,14 @@
-# ruff: file-ignore[magic-value-comparison]  # ruff: ignore[unused-noqa]
+# ruff: file-ignore[magic-value-comparison]
 from pathlib import Path
 
 from solution import part_one, part_two
 
-SAMPLE_PATH = Path(__file__).parent / 'sample_input.txt'
+DIR = Path(__file__).parent
 
 
 def test_part_one_with_sample_input() -> None:
-    assert part_one(SAMPLE_PATH) == 0
+    assert part_one(DIR / 'sample_input1.txt') == 12
 
 
 def test_part_two_with_sample_input() -> None:
-    assert part_two(SAMPLE_PATH) == 0
+    assert part_two(DIR / 'sample_input2.txt') == 4
