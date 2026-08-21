63e7a6ef2229b45c61dac31f297eebbbf2c83e3f
Faria22 <fariafelipe22@hotmail.com>
2026-08-20 22:18:48 -0500

    solution 2016 day 7

--- years/2016/day07/solution.py
+++ years/2016/day07/solution.py
@@ -1,3 +1,4 @@
+import re
 from pathlib import Path
 
 INPUT_PATH = Path(__file__).parent / 'input.txt'
@@ -9,14 +10,40 @@ def parse_data(input_path: Path) -> list[str]:
 
 def part_one(input_path: Path) -> int:
     """Return the answer to part one."""
-    data = parse_data(input_path)  # ruff: ignore[unused-variable]
-    return 0
+    lines = parse_data(input_path)
+    pattern = re.compile(r'(\[?)\w*(\w)(?!\2)(\w)\3\2')
+
+    ip_counts = 0
+    for line in lines:
+        matches = pattern.findall(line)
+        if not any(match[0] == '[' for match in matches) and any(match[0] != '[' for match in matches):
+            ip_counts += 1
+    return ip_counts
 
 
 def part_two(input_path: Path) -> int:
     """Return the answer to part two."""
-    data = parse_data(input_path)  # ruff: ignore[unused-variable]
-    return 0
+    lines = parse_data(input_path)
+    pattern = re.compile(r'(?=([a-z])(?!\1)([a-z])\1)')
+
+    ip_counts = 0
+    for line in lines:
+        sections = re.split(r'[\[\]]', line)
+        outside_sections = sections[::2]
+        inside_sections = sections[1::2]
+
+        outside_matches = []
+        for section in outside_sections:
+            outside_matches.extend(match for match in pattern.findall(section) if match)
+
+        inside_matches = []
+        for section in inside_sections:
+            inside_matches.extend(match for match in pattern.findall(section) if match)
+
+        if any(a == d and b == c for a, b in outside_matches for c, d in inside_matches):
+            ip_counts += 1
+
+    return ip_counts
 
 
 def main() -> None:
