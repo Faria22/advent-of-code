b809d527c9881e52b5a3db4ed7c4585bda0a9367
Faria22 <fariafelipe22@hotmail.com>
2026-08-20 20:08:12 -0500

    solution 2016 day 5

--- years/2016/day05/solution.py
+++ years/2016/day05/solution.py
@@ -1,22 +1,54 @@
+import hashlib
 from pathlib import Path
 
 INPUT_PATH = Path(__file__).parent / 'input.txt'
 
 
-def parse_data(input_path: Path) -> list[str]:
-    return input_path.read_text().strip().split('\n')
+PASSWORD_LEN = 8
 
 
-def part_one(input_path: Path) -> int:
+def parse_data(input_path: Path) -> str:
+    return input_path.read_text().strip()
+
+
+def part_one(input_path: Path) -> str:
     """Return the answer to part one."""
-    data = parse_data(input_path)  # ruff: ignore[unused-variable]
-    return 0
+    key = parse_data(input_path)
+    password = ''
+
+    num = 1
+    for _ in range(PASSWORD_LEN):
+        while not (hash_str := hashlib.md5((key + str(num)).encode()).hexdigest()).startswith('00000'):
+            num += 1
+
+        password += hash_str[5]
+        num += 1
+
+    return password
 
 
-def part_two(input_path: Path) -> int:
+def part_two(input_path: Path) -> str:
     """Return the answer to part two."""
-    data = parse_data(input_path)  # ruff: ignore[unused-variable]
-    return 0
+    key = parse_data(input_path)
+    password = [''] * PASSWORD_LEN
+
+    num = 1
+    seen = set()
+    while len(seen) != PASSWORD_LEN:
+        while not (hash_str := hashlib.md5((key + str(num)).encode()).hexdigest()).startswith('00000'):
+            num += 1
+
+        idx, val = hash_str[5:7]
+        try:
+            if 0 <= (idx := int(idx)) < PASSWORD_LEN and idx not in seen:
+                seen.add(idx)
+                password[idx] = val
+        except ValueError:
+            pass
+
+        num += 1
+
+    return ''.join(password)
 
 
 def main() -> None:
--- years/2016/day05/test.py
+++ years/2016/day05/test.py
@@ -1,4 +1,3 @@
-# ruff: file-ignore[magic-value-comparison]  # ruff: ignore[unused-noqa]
 from pathlib import Path
 
 from solution import part_one, part_two
@@ -7,8 +6,8 @@ SAMPLE_PATH = Path(__file__).parent / 'sample_input.txt'
 
 
 def test_part_one_with_sample_input() -> None:
-    assert part_one(SAMPLE_PATH) == 0
+    assert part_one(SAMPLE_PATH) == '18f47a30'
 
 
 def test_part_two_with_sample_input() -> None:
-    assert part_two(SAMPLE_PATH) == 0
+    assert part_two(SAMPLE_PATH) == '05ace8e3'
