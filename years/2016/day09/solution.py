9c80d7503d798eb6af6209931df39e361cab445d
Faria22 <fariafelipe22@hotmail.com>
2026-08-22 09:49:06 -0500

    solution 2016 day 9

--- years/2016/day09/solution.py
+++ years/2016/day09/solution.py
@@ -1,22 +1,63 @@
+import re
 from pathlib import Path
 
 INPUT_PATH = Path(__file__).parent / 'input.txt'
 
 
-def parse_data(input_path: Path) -> list[str]:
-    return input_path.read_text().strip().split('\n')
+def parse_data(input_path: Path) -> str:
+    return input_path.read_text().strip()
+
+
+def decompress_line_length(line: str) -> int:
+    pattern = re.compile(r'\((\d+)x(\d+)\)')
+    new_line_len = 0
+    while line:
+        match = pattern.search(line)
+        if match is None:
+            new_line_len += len(line)
+            line = ''
+            continue
+
+        before, after = line[: match.start()], line[match.end() :]
+        num_chars, num_times = int(match[1]), int(match[2])
+
+        new_line_len += len(before)
+        new_line_len += num_chars * num_times
+        line = after[num_chars:]
+
+    return new_line_len
+
+
+def decompress_line_length_v2(line: str) -> int:
+    pattern = re.compile(r'\((\d+)x(\d+)\)')
+    new_line_len = 0
+    while line:
+        match = pattern.search(line)
+        if match is None:
+            new_line_len += len(line)
+            line = ''
+            continue
+
+        before, after = line[: match.start()], line[match.end() :]
+        num_chars, num_times = int(match[1]), int(match[2])
+
+        new_line_len += len(before)
+        new_line_len += num_times * decompress_line_length_v2(after[:num_chars])
+        line = after[num_chars:]
+
+    return new_line_len
 
 
 def part_one(input_path: Path) -> int:
     """Return the answer to part one."""
-    data = parse_data(input_path)  # ruff: ignore[unused-variable]
-    return 0
+    line = parse_data(input_path)
+    return decompress_line_length(line)
 
 
 def part_two(input_path: Path) -> int:
     """Return the answer to part two."""
-    data = parse_data(input_path)  # ruff: ignore[unused-variable]
-    return 0
+    line = parse_data(input_path)
+    return decompress_line_length_v2(line)
 
 
 def main() -> None:
--- years/2016/day09/test.py
+++ years/2016/day09/test.py
@@ -1,14 +1,23 @@
-# ruff: file-ignore[magic-value-comparison]  # ruff: ignore[unused-noqa]
+# ruff: file-ignore[magic-value-comparison]
 from pathlib import Path
 
 from solution import part_one, part_two
 
-SAMPLE_PATH = Path(__file__).parent / 'sample_input.txt'
+
+def sample_path(n: int) -> Path:
+    return Path(__file__).parent / f'sample_input{n}.txt'
 
 
 def test_part_one_with_sample_input() -> None:
-    assert part_one(SAMPLE_PATH) == 0
+    assert part_one(sample_path(1)) == 7
+    assert part_one(sample_path(2)) == 9
+    assert part_one(sample_path(3)) == 11
+    assert part_one(sample_path(4)) == 6
+    assert part_one(sample_path(5)) == 18
 
 
 def test_part_two_with_sample_input() -> None:
-    assert part_two(SAMPLE_PATH) == 0
+    assert part_two(sample_path(1)) == 7
+    assert part_two(sample_path(5)) == 20
+    assert part_two(sample_path(6)) == 241920
+    assert part_two(sample_path(7)) == 445
