54c3df1f1ce4a2f79f727e730310d6a3819c2dcb
Faria22 <fariafelipe22@hotmail.com>
2026-08-24 21:33:40 -0500

    solution 2016 day 15

--- years/2016/day15/solution.py
+++ years/2016/day15/solution.py
@@ -3,20 +3,38 @@ from pathlib import Path
 INPUT_PATH = Path(__file__).parent / 'input.txt'
 
 
-def parse_data(input_path: Path) -> list[str]:
-    return input_path.read_text().strip().split('\n')
+def parse_data(input_path: Path) -> list[tuple[int, int]]:
+    disks = []
+    for line in input_path.read_text().strip().split('\n'):
+        parts = line.split()
+        num_pos = int(parts[3])
+        cur_pos = int(parts[-1][:-1])
+        disks.append((num_pos, cur_pos))
+    return disks
+
+
+def passes_all_disks(t: int, disks: list[tuple[int, int]]) -> bool:
+    return all((cur_pos + disk_idx + t) % num_pos == 0 for disk_idx, (num_pos, cur_pos) in enumerate(disks, 1))
 
 
 def part_one(input_path: Path) -> int:
     """Return the answer to part one."""
-    data = parse_data(input_path)  # ruff: ignore[unused-variable]
-    return 0
+    disks = parse_data(input_path)
+    t = 0
+    while not passes_all_disks(t, disks):
+        t += 1
+
+    return t
 
 
 def part_two(input_path: Path) -> int:
     """Return the answer to part two."""
-    data = parse_data(input_path)  # ruff: ignore[unused-variable]
-    return 0
+    disks = [*parse_data(input_path), (11, 0)]
+    t = 0
+    while not passes_all_disks(t, disks):
+        t += 1
+
+    return t
 
 
 def main() -> None:
