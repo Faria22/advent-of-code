a99c40202481606b93ac7513250408c1a0f0e55e
Faria22 <fariafelipe22@hotmail.com>
2026-08-27 22:49:12 -0500

    solution 2016 day 18

--- years/2016/day18/solution.py
+++ years/2016/day18/solution.py
@@ -2,21 +2,46 @@ from pathlib import Path
 
 INPUT_PATH = Path(__file__).parent / 'input.txt'
 
-
-def parse_data(input_path: Path) -> list[str]:
-    return input_path.read_text().strip().split('\n')
+SAFE_TILE = '.'
 
 
-def part_one(input_path: Path) -> int:
+def get_next_row(cur_row: int, num_tiles: int) -> int:
+    left_neighbors = cur_row << 1
+    right_neighbors = cur_row >> 1
+
+    next_row = left_neighbors ^ right_neighbors
+    mask = (1 << num_tiles) - 1
+    return next_row & mask
+
+
+def parse_data(input_path: Path) -> tuple[int, int]:
+    row = 0
+    for idx, tile in enumerate(input_path.read_text().strip()):
+        if tile != SAFE_TILE:
+            row += 1 << idx
+    return row, idx + 1
+
+
+def part_one(input_path: Path, num_rows: int = 40) -> int:
     """Return the answer to part one."""
-    data = parse_data(input_path)  # ruff: ignore[unused-variable]
-    return 0
+    row, num_tiles = parse_data(input_path)
+    safe_tile_count = num_tiles - row.bit_count()
+    for _ in range(num_rows - 1):
+        row = get_next_row(row, num_tiles)
+        safe_tile_count += num_tiles - row.bit_count()
+
+    return safe_tile_count
 
 
 def part_two(input_path: Path) -> int:
     """Return the answer to part two."""
-    data = parse_data(input_path)  # ruff: ignore[unused-variable]
-    return 0
+    row, num_tiles = parse_data(input_path)
+    safe_tile_count = num_tiles - row.bit_count()
+    for _ in range(400000 - 1):
+        row = get_next_row(row, num_tiles)
+        safe_tile_count += num_tiles - row.bit_count()
+
+    return safe_tile_count
 
 
 def main() -> None:
