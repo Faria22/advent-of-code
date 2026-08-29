d6ce67af45a861718cfd5e12936e53b64c2a04b8
Faria22 <fariafelipe22@hotmail.com>
2026-08-28 20:53:53 -0500

    solution 2016 day 19

--- years/2016/day19/solution.py
+++ years/2016/day19/solution.py
@@ -3,20 +3,25 @@ from pathlib import Path
 INPUT_PATH = Path(__file__).parent / 'input.txt'
 
 
-def parse_data(input_path: Path) -> list[str]:
-    return input_path.read_text().strip().split('\n')
+def parse_data(input_path: Path) -> int:
+    return int(input_path.read_text().strip())
 
 
 def part_one(input_path: Path) -> int:
     """Return the answer to part one."""
-    data = parse_data(input_path)  # ruff: ignore[unused-variable]
-    return 0
+    num_elfs = parse_data(input_path)
+    power = num_elfs.bit_length() - 1
+    d = num_elfs - 2**power
+    return 2 * d + 1
 
 
 def part_two(input_path: Path) -> int:
     """Return the answer to part two."""
-    data = parse_data(input_path)  # ruff: ignore[unused-variable]
-    return 0
+    num_elfs = parse_data(input_path)
+    max_power = 0
+    while 3**max_power <= num_elfs:
+        max_power += 1
+    return num_elfs - 3 ** (max_power - 1)
 
 
 def main() -> None:
