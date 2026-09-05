7da5d109791f8dc0458f660c5ed05430cc7bf553
Faria22 <fariafelipe22@hotmail.com>
2026-09-05 18:21:34 -0500

    solution 2017 day 1

--- years/2017/day01/solution.py
+++ years/2017/day01/solution.py
@@ -1,22 +1,32 @@
+import re
 from pathlib import Path
 
 INPUT_PATH = Path(__file__).parent / 'input.txt'
 
 
-def parse_data(input_path: Path) -> list[str]:
-    return input_path.read_text().strip().split('\n')
+def parse_data(input_path: Path) -> str:
+    return input_path.read_text().strip()
 
 
 def part_one(input_path: Path) -> int:
     """Return the answer to part one."""
-    data = parse_data(input_path)  # ruff: ignore[unused-variable]
-    return 0
+    num = parse_data(input_path)
+    num += num[0]
+    pattern = re.compile(r'(?=(\d)\1)')
+    return sum(int(match) for match in pattern.findall(num))
+
+
+def get_shifted_char(input_str: str, idx: int) -> str:
+    input_len = len(input_str)
+    shift = input_len // 2
+    new_idx = (idx + shift) % input_len
+    return input_str[new_idx]
 
 
 def part_two(input_path: Path) -> int:
     """Return the answer to part two."""
-    data = parse_data(input_path)  # ruff: ignore[unused-variable]
-    return 0
+    num = parse_data(input_path)
+    return sum(int(char) for idx, char in enumerate(num) if char == get_shifted_char(num, idx))
 
 
 def main() -> None:
