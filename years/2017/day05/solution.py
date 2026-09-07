5a311a610ef7da9826dff4b57a78edb1f3d3dd99
Faria22 <fariafelipe22@hotmail.com>
2026-09-07 09:44:51 -0500

    solution 2017 day 5

--- years/2017/day05/solution.py
+++ years/2017/day05/solution.py
@@ -3,20 +3,43 @@ from pathlib import Path
 INPUT_PATH = Path(__file__).parent / 'input.txt'
 
 
-def parse_data(input_path: Path) -> list[str]:
-    return input_path.read_text().strip().split('\n')
+def parse_data(input_path: Path) -> list[int]:
+    return [int(num) for num in input_path.read_text().strip().split('\n')]
 
 
 def part_one(input_path: Path) -> int:
     """Return the answer to part one."""
-    data = parse_data(input_path)  # ruff: ignore[unused-variable]
-    return 0
+    instructions = parse_data(input_path)
+    num_instructions = len(instructions)
+    instructions = dict(enumerate(instructions))
+    num_steps = 0
+    idx = 0
+    while idx < num_instructions:
+        num_steps += 1
+        jump = instructions[idx]
+        instructions[idx] += 1
+        idx += jump
+
+    return num_steps
 
 
 def part_two(input_path: Path) -> int:
     """Return the answer to part two."""
-    data = parse_data(input_path)  # ruff: ignore[unused-variable]
-    return 0
+    instructions = parse_data(input_path)
+    num_instructions = len(instructions)
+    instructions = dict(enumerate(instructions))
+    num_steps = 0
+    idx = 0
+    while idx < num_instructions:
+        num_steps += 1
+        jump = instructions[idx]
+        if jump >= 3:  # ruff: ignore[magic-value-comparison]
+            instructions[idx] -= 1
+        else:
+            instructions[idx] += 1
+        idx += jump
+
+    return num_steps
 
 
 def main() -> None:
