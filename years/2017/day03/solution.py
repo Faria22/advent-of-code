b1a3472dcd7de0a53352695efadb754a440230fe
Faria22 <fariafelipe22@hotmail.com>
2026-09-06 21:17:30 -0500

    solution 2017 day 3

--- years/2017/day03/solution.py
+++ years/2017/day03/solution.py
@@ -1,27 +1,85 @@
 from pathlib import Path
 
+from aoc import Pos
+
 INPUT_PATH = Path(__file__).parent / 'input.txt'
 
 
-def parse_data(input_path: Path) -> list[str]:
-    return input_path.read_text().strip().split('\n')
+def parse_data(input_path: Path) -> int:
+    return int(input_path.read_text().strip())
 
 
-def part_one(input_path: Path) -> int:
+def get_location_pos(location: int) -> Pos:
+    if location == 1:
+        return Pos(0, 0)
+
+    ring = 3
+    while ring**2 < location:
+        ring += 2
+
+    start_row = (ring - 3) // 2
+    start_col = start_row + 1
+
+    if location == ring**2:
+        return Pos(start_row + 1, start_col)
+
+    start_location = (ring - 2) ** 2 + 1
+    num_locations_up = ring - 2
+    num_locations_left = num_locations_down = num_locations_right = num_locations_up + 1
+
+    # Check if location is above start of the ring
+    if start_location <= location <= start_location + num_locations_up:
+        shift = location - start_location
+        return Pos(start_row - shift, start_col)
+
+    start_row -= num_locations_up
+    start_location += num_locations_up
+
+    # Check if location is above and left start of the ring
+    if start_location <= location <= start_location + num_locations_left:
+        shift = location - start_location
+        return Pos(start_row, start_col - shift)
+
+    start_col -= num_locations_left
+    start_location += num_locations_left
+
+    # Check if location is above, left, and down start of the ring
+    if start_location <= location <= start_location + num_locations_down:
+        shift = location - start_location
+        return Pos(start_row + shift, start_col)
+
+    start_row += num_locations_down
+    start_location += num_locations_down
+
+    # Check if location is above, left, down, right start of the ring
+    if start_location <= location <= start_location + num_locations_right:
+        shift = location - start_location
+        return Pos(start_row, start_col + shift)
+
+    raise RuntimeError
+
+
+def part_one(location: int = parse_data(INPUT_PATH)) -> int:
     """Return the answer to part one."""
-    data = parse_data(input_path)  # ruff: ignore[unused-variable]
-    return 0
+    return sum(abs(x) for x in get_location_pos(location))
 
 
-def part_two(input_path: Path) -> int:
+def part_two(input_val: int = parse_data(INPUT_PATH)) -> int:
     """Return the answer to part two."""
-    data = parse_data(input_path)  # ruff: ignore[unused-variable]
+    location_values = {Pos(0, 0): 1}
+    for location in range(2, input_val**2):
+        pos = get_location_pos(location)
+        if (val := sum(location_values.get(neighbor, 0) for neighbor in pos.neighbors(diagonals=True))) > input_val:
+            return val
+
+        location_values[pos] = val
+
     return 0
 
 
 def main() -> None:
-    print(f'Part 1: {part_one(INPUT_PATH)}')
-    print(f'Part 2: {part_two(INPUT_PATH)}')
+    print(f'Part 1: {part_one()}')
+    print(f'Part 2: {part_two()}')
 
 
 if __name__ == '__main__':
--- years/2017/day03/test.py
+++ years/2017/day03/test.py
@@ -1,4 +1,4 @@
-# ruff: file-ignore[magic-value-comparison]  # ruff: ignore[unused-noqa]
+# ruff: file-ignore[magic-value-comparison]
 from pathlib import Path
 
 from solution import part_one, part_two
@@ -7,8 +7,23 @@ SAMPLE_PATH = Path(__file__).parent / 'sample_input.txt'
 
 
 def test_part_one_with_sample_input() -> None:
-    assert part_one(SAMPLE_PATH) == 0
+    assert part_one(1) == 0
+    assert part_one(3) == 2
+    assert part_one(9) == 2
+    assert part_one(12) == 3
+    assert part_one(13) == 4
+    assert part_one(17) == 4
+    assert part_one(23) == 2
+    assert part_one(25) == 4
+    assert part_one(26) == 5
+    assert part_one(1024) == 31
 
 
 def test_part_two_with_sample_input() -> None:
-    assert part_two(SAMPLE_PATH) == 0
+    assert part_two(800) == 806
+    assert part_two(24) == 25
+    assert part_two(3) == 4
+    assert part_two(130) == 133
+    assert part_two(315) == 330
+    assert part_two(300) == 304
+    assert part_two(50) == 54
