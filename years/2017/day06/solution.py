e6e6ace7ed060c3e2e2448a80ea04989c3bfa5ac
Faria22 <fariafelipe22@hotmail.com>
2026-09-08 18:04:57 -0500

    solution 2017 day 6

--- years/2017/day06/solution.py
+++ years/2017/day06/solution.py
@@ -1,22 +1,65 @@
+import operator
 from pathlib import Path
 
 INPUT_PATH = Path(__file__).parent / 'input.txt'
 
+type Banks = tuple[int, ...]
 
-def parse_data(input_path: Path) -> list[str]:
-    return input_path.read_text().strip().split('\n')
+
+def parse_data(input_path: Path) -> Banks:
+    return tuple(int(n) for n in input_path.read_text().strip().split())
+
+
+def next_state(banks: Banks) -> Banks:
+    num_banks = len(banks)
+    max_idx, max_val = max(enumerate(banks), key=operator.itemgetter(1))
+
+    overall_shift, partial_shift = divmod(max_val, num_banks)
+
+    new_banks = [num + overall_shift if idx != max_idx else overall_shift for idx, num in enumerate(banks)]
+
+    spaces_to_the_right = num_banks - max_idx - 1
+    shift_to_the_right = partial_shift
+    if partial_shift > spaces_to_the_right:
+        wraped_shift = partial_shift - spaces_to_the_right
+        shift_to_the_right -= wraped_shift
+        for i in range(wraped_shift):
+            new_banks[i] += 1
+
+    # Does the spaces to the right
+    for i in range(max_idx + 1, max_idx + 1 + shift_to_the_right):
+        new_banks[i] += 1
+
+    return tuple(new_banks)
 
 
 def part_one(input_path: Path) -> int:
     """Return the answer to part one."""
-    data = parse_data(input_path)  # ruff: ignore[unused-variable]
-    return 0
+    banks = parse_data(input_path)
+    num_steps = 0
+    seen_banks = set()
+    while banks not in seen_banks:
+        seen_banks.add(banks)
+        num_steps += 1
+        banks = next_state(banks)
+    return num_steps
 
 
 def part_two(input_path: Path) -> int:
     """Return the answer to part two."""
-    data = parse_data(input_path)  # ruff: ignore[unused-variable]
-    return 0
+    banks = parse_data(input_path)
+    seen_banks = set()
+    while banks not in seen_banks:
+        seen_banks.add(banks)
+        banks = next_state(banks)
+
+    target_banks = banks
+    num_steps = 1
+    banks = next_state(banks)
+    while banks != target_banks:
+        num_steps += 1
+        banks = next_state(banks)
+    return num_steps
 
 
 def main() -> None:
