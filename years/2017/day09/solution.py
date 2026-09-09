dd11e5301d2c90440a4d66a18967cd03da4144ac
Faria22 <fariafelipe22@hotmail.com>
2026-09-08 22:42:51 -0500

    solution 2017 day 9

--- years/2017/day09/solution.py
+++ years/2017/day09/solution.py
@@ -1,27 +1,52 @@
+import re
 from pathlib import Path
 
 INPUT_PATH = Path(__file__).parent / 'input.txt'
 
 
-def parse_data(input_path: Path) -> list[str]:
-    return input_path.read_text().strip().split('\n')
+def parse_data(input_path: Path) -> str:
+    return input_path.read_text().strip()
 
 
-def part_one(input_path: Path) -> int:
+def score_groups(input_str: str) -> int:
+    score = 0
+    group_idx = 0
+    for char in input_str:
+        match char:
+            case '{':
+                group_idx += 1
+                score += group_idx
+            case '}':
+                group_idx -= 1
+            case _:
+                raise RuntimeError
+    return score
+
+
+def part_one(input_str: str = parse_data(INPUT_PATH)) -> int:
     """Return the answer to part one."""
-    data = parse_data(input_path)  # ruff: ignore[unused-variable]
-    return 0
+    # remove exclamation points
+    input_str = re.sub(r'!.', '', input_str)
+
+    # remove garbage
+    input_str = re.sub(r'<.*?>', '', input_str)
+
+    # remove commas
+    input_str = input_str.replace(',', '')
+
+    return score_groups(input_str)
 
 
-def part_two(input_path: Path) -> int:
+def part_two(input_str: str = parse_data(INPUT_PATH)) -> int:
     """Return the answer to part two."""
-    data = parse_data(input_path)  # ruff: ignore[unused-variable]
-    return 0
+    input_str = re.sub(r'!.', '', input_str)
+    non_canceled_chars_in_garbage = ''.join(re.findall(r'<(.*?)>', input_str))
+    return len(non_canceled_chars_in_garbage)
 
 
 def main() -> None:
-    print(f'Part 1: {part_one(INPUT_PATH)}')
-    print(f'Part 2: {part_two(INPUT_PATH)}')
+    print(f'Part 1: {part_one()}')
+    print(f'Part 2: {part_two()}')
 
 
 if __name__ == '__main__':
--- years/2017/day09/test.py
+++ years/2017/day09/test.py
@@ -1,14 +1,24 @@
-# ruff: file-ignore[magic-value-comparison]  # ruff: ignore[unused-noqa]
-from pathlib import Path
+# ruff: file-ignore[magic-value-comparison]
 
 from solution import part_one, part_two
 
-SAMPLE_PATH = Path(__file__).parent / 'sample_input.txt'
-
 
 def test_part_one_with_sample_input() -> None:
-    assert part_one(SAMPLE_PATH) == 0
+    assert part_one('{}') == 1
+    assert part_one('{{{}}}') == 6
+    assert part_one('{{},{}}') == 5
+    assert part_one('{{{},{},{{}}}}') == 16
+    assert part_one('{<a>,<a>,<a>,<a>}') == 1
+    assert part_one('{{<ab>},{<ab>},{<ab>},{<ab>}}') == 9
+    assert part_one('{{<!!>},{<!!>},{<!!>},{<!!>}}') == 9
+    assert part_one('{{<a!>},{<a!>},{<a!>},{<ab>}}') == 3
 
 
 def test_part_two_with_sample_input() -> None:
-    assert part_two(SAMPLE_PATH) == 0
+    assert part_two('<>') == 0
+    assert part_two('<random characters>') == 17
+    assert part_two('<<<<>') == 3
+    assert part_two('<{!>}>') == 2
+    assert part_two('<!!>') == 0
+    assert part_two('<!!!>>') == 0
+    assert part_two('<{o"i!a,<{i<a>') == 10
