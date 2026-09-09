0530c978efb4cf728cc7a70a82e2f5431c411c32
Faria22 <fariafelipe22@hotmail.com>
2026-09-08 21:27:49 -0500

    solution 2017 day 7

--- years/2017/day07/solution.py
+++ years/2017/day07/solution.py
@@ -1,22 +1,68 @@
+from collections import Counter
+from dataclasses import dataclass
 from pathlib import Path
 
 INPUT_PATH = Path(__file__).parent / 'input.txt'
 
 
-def parse_data(input_path: Path) -> list[str]:
-    return input_path.read_text().strip().split('\n')
+@dataclass
+class Program:
+    weight: int
+    weight_under: int
+    children: set[str]
 
 
-def part_one(input_path: Path) -> int:
+def parse_data(input_path: Path) -> dict[str, Program]:
+    programs = {}
+    for line in input_path.read_text().strip().split('\n'):
+        program, *children_str = line.split(' -> ')
+
+        program, weight = program.split()
+        weight = int(weight[1:-1])
+
+        children = set(children_str[0].split(', ')) if children_str else set()
+
+        programs[program] = Program(weight, 0, children)
+
+    return programs
+
+
+def part_one(input_path: Path) -> str:
     """Return the answer to part one."""
-    data = parse_data(input_path)  # ruff: ignore[unused-variable]
-    return 0
+    programs = parse_data(input_path)
+    child_programs = {child for program in programs.values() for child in program.children}
+    return (programs.keys() - child_programs).pop()
 
 
 def part_two(input_path: Path) -> int:
     """Return the answer to part two."""
-    data = parse_data(input_path)  # ruff: ignore[unused-variable]
-    return 0
+    programs = parse_data(input_path)
+    while True:
+        for parent, program in programs.items():
+            # Only check programs above leaf programs
+            children_programs = {child: programs[child] for child in program.children}
+            if not children_programs or any(child_program.children for child_program in children_programs.values()):
+                continue
+
+            # All children have the same weight
+            children_weight = {
+                child: c_program.weight + c_program.weight_under for child, c_program in children_programs.items()
+            }
+            if len(set(children_weight.values())) == 1:
+                weight_under = 0
+                for child in children_programs:
+                    child_program = programs.pop(child)
+                    weight_under += child_program.weight + child_program.weight_under
+
+                programs[parent] = Program(program.weight, weight_under, set())
+                break
+
+            # Weights are different
+            (common, _), (different, _) = Counter(children_weight.values()).most_common()
+            difference = different - common
+            for child, weight in children_weight.items():
+                if weight == different:
+                    return programs[child].weight - difference
 
 
 def main() -> None:
