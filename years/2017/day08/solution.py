a05c5cb75bdf6516ad1d718bcf3e9d2746267140
Faria22 <fariafelipe22@hotmail.com>
2026-09-08 21:42:46 -0500

    solution 2017 day 8

--- years/2017/day08/solution.py
+++ years/2017/day08/solution.py
@@ -1,3 +1,5 @@
+import operator
+from collections import defaultdict
 from pathlib import Path
 
 INPUT_PATH = Path(__file__).parent / 'input.txt'
@@ -7,16 +9,57 @@ def parse_data(input_path: Path) -> list[str]:
     return input_path.read_text().strip().split('\n')
 
 
+def parse_instruction(instruction: str) -> tuple[str, int, str]:
+    register, sign, shift, _, condition = instruction.split(maxsplit=4)
+
+    shift = int(shift)
+    if sign == 'dec':
+        shift = -shift
+
+    return register, shift, condition
+
+
+def evaluate_condition(condition: str, registers: defaultdict[str, int]) -> bool:
+    op_map = {
+        '>': operator.gt,
+        '>=': operator.ge,
+        '<': operator.lt,
+        '<=': operator.le,
+        '==': operator.eq,
+        '!=': operator.ne,
+    }
+
+    register, op, val = condition.split()
+    val = int(val)
+
+    return op_map[op](registers[register], val)
+
+
 def part_one(input_path: Path) -> int:
     """Return the answer to part one."""
-    data = parse_data(input_path)  # ruff: ignore[unused-variable]
-    return 0
+    instructions = parse_data(input_path)
+    registers = defaultdict(int)
+    for instruction in instructions:
+        register, shift, condition = parse_instruction(instruction)
+        if evaluate_condition(condition, registers):
+            registers[register] += shift
+
+    return max(registers.values())
 
 
 def part_two(input_path: Path) -> int:
     """Return the answer to part two."""
-    data = parse_data(input_path)  # ruff: ignore[unused-variable]
-    return 0
+    instructions = parse_data(input_path)
+    registers = defaultdict(int)
+    max_val = 0
+    for instruction in instructions:
+        register, shift, condition = parse_instruction(instruction)
+        if evaluate_condition(condition, registers):
+            registers[register] += shift
+
+        max_val = max(max_val, *registers.values())
+
+    return max_val
 
 
 def main() -> None:
