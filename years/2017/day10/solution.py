bfec0f28de9d0a9755e21e713c1c9a863952fa2c
Faria22 <fariafelipe22@hotmail.com>
2026-09-09 21:57:52 -0500

    solution 2017 day 10

--- years/2017/day10/solution.py
+++ years/2017/day10/solution.py
@@ -1,22 +1,73 @@
+import operator
+from functools import reduce
 from pathlib import Path
 
 INPUT_PATH = Path(__file__).parent / 'input.txt'
 
-
-def parse_data(input_path: Path) -> list[str]:
-    return input_path.read_text().strip().split('\n')
+LENGTH = 256
 
 
-def part_one(input_path: Path) -> int:
+def parse_data(input_path: Path) -> list[int]:
+    return [int(n) for n in input_path.read_text().strip().split(',')]
+
+
+def translate_input(input_path: Path) -> list[int]:
+    lengths = [ord(char) for char in input_path.read_text().strip()]
+    return [*lengths, 17, 31, 73, 47, 23]
+
+
+def shift_list_left(list_: list, n: int) -> list:
+    return list_[n:] + list_[:n]
+
+
+def action(elements: list[int], idx: int, length: int) -> list[int]:
+    elements = shift_list_left(elements, idx)
+    elements[:length] = reversed(elements[:length])
+    return shift_list_left(elements, -idx)
+
+
+def single_round(elements: list[int], lengths: list[int], idx: int, skip_size: int) -> tuple[list[int], int, int]:
+    num_elements = len(elements)
+    for skip, length in enumerate(lengths, skip_size):
+        elements = action(elements, idx, length)
+        idx = (idx + length + skip) % num_elements
+
+    return elements, idx, skip + 1
+
+
+def get_sparse_hash(elements: list[int]) -> list[int]:
+    sparse_hash = []
+    for idx in range(16):
+        val = reduce(operator.xor, elements[idx * 16 : idx * 16 + 16])
+        sparse_hash.append(val)
+    return sparse_hash
+
+
+def get_knot_hash(sparse_hash: list[int]) -> str:
+    return ''.join(f'{n:02x}' for n in sparse_hash)
+
+
+def part_one(input_path: Path, num_elements: int = LENGTH) -> int:
     """Return the answer to part one."""
-    data = parse_data(input_path)  # ruff: ignore[unused-variable]
-    return 0
+    lengths = parse_data(input_path)
+    elements = list(range(num_elements))
+    elements, _, _ = single_round(elements, lengths, 0, 0)
+
+    return elements[0] * elements[1]
 
 
-def part_two(input_path: Path) -> int:
+def part_two(input_path: Path) -> str:
     """Return the answer to part two."""
-    data = parse_data(input_path)  # ruff: ignore[unused-variable]
-    return 0
+    lengths = translate_input(input_path)
+    elements = list(range(LENGTH))
+
+    idx = 0
+    skip_size = 0
+    for _ in range(64):
+        elements, idx, skip_size = single_round(elements, lengths, idx, skip_size)
+
+    sparse_hash = get_sparse_hash(elements)
+    return get_knot_hash(sparse_hash)
 
 
 def main() -> None:
--- years/2017/day10/test.py
+++ years/2017/day10/test.py
@@ -1,14 +1,31 @@
-# ruff: file-ignore[magic-value-comparison]  # ruff: ignore[unused-noqa]
+# ruff: file-ignore[magic-value-comparison]
 from pathlib import Path
 
-from solution import part_one, part_two
+from solution import get_knot_hash, get_sparse_hash, part_one, part_two, translate_input
 
-SAMPLE_PATH = Path(__file__).parent / 'sample_input.txt'
+DIR = Path(__file__).parent
 
 
 def test_part_one_with_sample_input() -> None:
-    assert part_one(SAMPLE_PATH) == 0
+    assert part_one(DIR / 'sample_input1.txt', 5) == 12
+
+
+def test_translate_input() -> None:
+    assert translate_input(DIR / 'sample_input4.txt') == [49, 44, 50, 44, 51, 17, 31, 73, 47, 23]
+
+
+def test_sparse_hash() -> None:
+    elements = [0] * 256
+    elements[:16] = [65, 27, 9, 1, 4, 3, 40, 50, 91, 7, 6, 0, 2, 5, 68, 22]
+    assert get_sparse_hash(elements)[0] == 64
+
+
+def test_knot_hash() -> None:
+    assert get_knot_hash([64, 7, 255]) == '4007ff'
 
 
 def test_part_two_with_sample_input() -> None:
-    assert part_two(SAMPLE_PATH) == 0
+    assert part_two(DIR / 'sample_input2.txt') == 'a2582a3a0e66e6e86e3812dcb672a272'
+    assert part_two(DIR / 'sample_input3.txt') == '33efeb34ea91902bb2f59c9920caa6cd'
+    assert part_two(DIR / 'sample_input4.txt') == '3efbe78a8d82f29979031a4aa0b16a9d'
+    assert part_two(DIR / 'sample_input5.txt') == '63960835bcdc130f0b66d7ff4f6a5a8e'
